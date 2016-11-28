fa7862c5e6485ea3f8a32da19c0007e4c84ecfed
Nick Petosa <nick.petosa@gmail.com>
2016-11-28 17:30:55 -0500

    Added unique indexing for the 'symbol' field of the 'molecules' collection

--- MongoQCDB.py
+++ MongoQCDB.py
@@ -10,3 +10,24 @@ class MongoQCDB:
 
     def find_one(self, coll):
         return self.db[coll].find_one()
+
+    def make_collections(self):
+        # Success dictionary and collections to create
+        success ={}
+        collection_creation = {}
+        collections = {"molecules", "databases", "pages"}
+
+        # Try to create a collection for each entry
+        for stri in collections:
+            try:
+                self.db.create_collection(stri)
+                collection_creation[stri] = 1
+            except pymongo.errors.CollectionInvalid:
+                collection_creation[stri] = 0
+        success["collection_creation"] = collection_creation
+
+        # Create a unique index on the "symbol" field for the "molecules" collection
+        success["create_index"] = self.db["molecules"].create_index("symbol", unique=True, background=True)
+
+        # Return the success array, where a value of 1 means successful
+        return success
--- main.py
+++ main.py
@@ -2,4 +2,4 @@ from MongoQCDB import MongoQCDB
 
 mongo = MongoQCDB("localhost", 27017, "local")
 
-print(mongo.find_one("temp"))
+print(mongo.make_collections())
