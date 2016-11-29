d3f13228589f72f77e95867b3e1118d209efb7c8
Nick Petosa <nick.petosa@gmail.com>
2016-11-29 00:10:15 -0500

    Finished JSON insertion wrapper

--- MongoQCDB.py
+++ MongoQCDB.py
@@ -2,22 +2,71 @@ import pymongo
 
 class MongoQCDB:
 
+    # Constructor
     def __init__(self, url, port, db):
         self.url = url
         self.port = port
-        client = pymongo.MongoClient(url, port)
-        self.db = client[db]
+        self.client = pymongo.MongoClient(url, port)
+        self.setup = self.init_db(db)
 
-    def find_one(self, coll):
-        return self.db[coll].find_one()
+    # Adds a molecule to the DB. Returns True on success, False on failure.
+    def add_molecule(self, symbol, name, geometry):
+        try:
+            self.db["molecules"].insert_one(
+                {
+                    "symbol" : symbol,
+                    "name" : name,
+                    "geometry" : geometry
+                    }
+                )
+            return True
+        except pymongo.errors.DuplicateKeyError:
+            return False
 
-    def make_collections(self):
+    # Adds a database to the DB. Returns True on success.
+    def add_database(self, name, reactions, citation, link):
+        try:
+            self.db["databases"].insert_one(
+                {
+                    "name" : name,
+                    "reactions" : reactions,
+                    "citation" : citation,
+                    "link" : link
+                }
+            )
+            return True
+        except pymongo.errors.DuplicateKeyError:
+            return False
+
+    # Adds a page to the DB. Returns True on success.
+    def add_page(self, molecule, method, value1, value2, value3, citation, link):
+        try:
+            self.db["pages"].insert_one(
+                {
+                    "molecule" : molecule,
+                    "method" : method,
+                    "value1" : value1,
+                    "value2" : value2,
+                    "value3" : value3,
+                    "citation" : citation,
+                    "link" : link
+                }
+            )
+            return True
+        except pymongo.errors.DuplicateKeyError:
+            return False
+
+    def init_db(self, db):
         # Success dictionary and collections to create
-        success ={}
+        success = {}
         collection_creation = {}
-        collections = {"molecules", "databases", "pages"}
+
+        # Create DB
+        self.db = self.client[db]
+        success["db"] = self.db
 
         # Try to create a collection for each entry
+        collections = {"molecules", "databases", "pages"}
         for stri in collections:
             try:
                 self.db.create_collection(stri)
@@ -27,7 +76,12 @@ class MongoQCDB:
         success["collection_creation"] = collection_creation
 
         # Create a unique index on the "symbol" field for the "molecules" collection
-        success["create_index"] = self.db["molecules"].create_index("symbol", unique=True, background=True)
+        success["molecules_index"] = self.db["molecules"].create_index("symbol", unique=True, background=True)
+        # Create a unique index on the "name" field for the "datbases" collection
+        success["databases_index"] = self.db["databases"].create_index("name", unique=True, background=True)
+        # Create a compound unique index on the "molecule"-"method" field for the "pages" collection
+        success["pages_index"] = self.db["pages"].create_index([("molecule", 1), ("method", 1)],
+                                                                unique=True, background=True)
 
         # Return the success array, where a value of 1 means successful
         return success
--- main.py
+++ main.py
@@ -1,5 +1,32 @@
 from MongoQCDB import MongoQCDB
+import json
+import os
 
 mongo = MongoQCDB("localhost", 27017, "local")
+print(mongo.setup)
 
-print(mongo.make_collections())
+# Add all JSON molecules
+fn = os.path.dirname(os.path.abspath(__file__)) + "/sample-json/molecules/"
+for filename in os.listdir(fn):
+    json_data = open(fn + filename).read()
+    data = json.loads(json_data)
+    inserted = mongo.add_molecule(data["symbol"], data["name"], data["geometry"])
+    print("Added molecule: " + data["symbol"] + ". Success=" + str(inserted))
+
+# Add all JSON databases
+fn = os.path.dirname(os.path.abspath(__file__)) + "/sample-json/databases/"
+for filename in os.listdir(fn):
+    json_data = open(fn + filename).read()
+    data = json.loads(json_data)
+    inserted = mongo.add_database(data["name"], data["reactions"], data["citation"], data["link"])
+    print("Added database: " + data["name"] + ". Success=" + str(inserted))
+
+# Add all JSON pages
+fn = os.path.dirname(os.path.abspath(__file__)) + "/sample-json/pages/"
+for filename in os.listdir(fn):
+    json_data = open(fn + filename).read()
+    data = json.loads(json_data)
+    inserted = mongo.add_page(data["molecule"], data["method"], data["value_1"],
+                       data["value_2"], data["value_3"], data["citation"],
+                       data["link"])
+    print("Added page: " + data["molecule"] + "-" + data["method"] + ". Success=" + str(inserted))
