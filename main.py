357f2803556739a2745e312424904dd0910de143
Nick Petosa <nick.petosa@gmail.com>
2016-11-30 20:04:23 -0500

    Added fragments to molecule, added hash fields for each collection

--- main.py
+++ main.py
@@ -8,17 +8,32 @@ mongo = MongoQCDB("localhost", 27017, "local")
 print(mongo.setup)
 
 m = hashlib.sha1()
-colls = ["molecules", "databases", "pages"]
+collections = ["molecules", "databases", "pages"]
+
+# For each collection, how to hash its JSON.
+hash_fields = {}
+hash_fields["molecules"] = ["symbols", "masses", "name", "charge", "multiplicity", "ghost", "geometry", "fragments"]
+hash_fields["databases"] = ["name"]
+hash_fields["pages"] = ["molecule", "method"]
+
+# Define the descriptor field for each collection. Used for logging.
+descriptor = {"molecules": "name", "databases": "name", "pages": "method"}
 
 # Add all JSON
-for col in colls:
+for col in collections:
     fn = os.path.dirname(os.path.abspath(__file__)) + "/sample-json/" + col + "/"
     for filename in os.listdir(fn):
         json_data = open(fn + filename).read()
         # Load JSON from file into OrderedDict
         data = json.loads(json_data, object_pairs_hook=OrderedDict)
-        # Load structured string into hashlib
-        m.update(json.dumps(data).encode("utf-8"))
+
+        # Update hashlib object based on hased fields
+        concat = ""
+        for field in hash_fields[col]:
+            concat += json.dumps(data[field])
+        m.update(concat.encode("utf-8"))
+
+        # Calculate SHA1 with hashlib
         digest = m.hexdigest()
         inserted = mongo.add_json(col, data, digest)
-        print("Added " + digest + " to " + col + ". Success=" + str(inserted) + ".")
+        print("[" + col + "] Added " + data[descriptor[col]] + " to " + col + " at _id " + digest + ". Success=" + str(inserted) + ".")
