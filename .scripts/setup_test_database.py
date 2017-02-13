a7a5d4073a871b6fdade2453b75c1dca7afd150b
Daniel Smith <malorian@me.com>
2017-02-13 10:33:23 -0500

    Restructuring database

--- .scripts/setup_test_database.py
+++ .scripts/setup_test_database.py
@@ -5,7 +5,8 @@ import json
 import os
 
 db_helper = db_helper.db_helper
-mongo = db_helper("192.168.2.139", 27017, "local")
+#mongo = db_helper("192.168.2.139", 27017, "local")
+mongo = db_helper("127.0.0.1", 27017, "local")
 print(mongo.setup)
 
 collections = ["molecules", "databases", "pages"]
