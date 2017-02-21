031f85367d567d94e482cffb0208458d85d7afbe
Nick Petosa <nick.petosa@gmail.com>
2017-02-20 23:11:27 -0500

    Removed sample file

--- mongo_qcdb/sample.py
+++
@@ -1,6 +0,0 @@
-import mongo_qcdb as mdb
-
-mongo = mdb.mongo_socket.MongoDB("127.0.0.1", 27017, "local")
-
-wpbe_val = mongo.get_dataframe("variables.CURRENT DIPOLE Y", "HBC6", "cp", ["WPBE/qzvp", "B3LYP/aug-cc-pVDZ"], do_stoich=True, debug_level=1)
-print(wpbe_val)
