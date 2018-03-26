eeaa18829a6757f37ae1f5b667dd84a1b55688cb
Daniel Smith <malorian@me.com>
2018-03-26 17:09:33 -0400

    More cleanup in client purge

--- dqm_server/__init__.py
+++ dqm_server/__init__.py
@@ -3,19 +3,19 @@ Main init function for dqm_server
 """
 
 # Import modules
-from . import mongo_helper
-from . import database
-from . import test_util
-from . import constants
-from . import visualization
-from . import handlers
-from . import compute
-
-# Move classes up a level
-from .molecule import Molecule
-from .database import Database
-from .client import Client
-from .mongo_helper import MongoSocket
+# from . import mongo_helper
+# from . import database
+# from . import test_util
+# from . import constants
+# from . import visualization
+# from . import handlers
+# from . import compute
+# 
+# # Move classes up a level
+# from .molecule import Molecule
+# from .database import Database
+# from .client import Client
+# from .mongo_helper import MongoSocket
 
 # Handle versioneer
 from ._version import get_versions
