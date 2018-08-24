6812cb8842cfd6406743d4415c98d70d0970227b
Daniel Smith <malorian@me.com>
2018-08-24 10:33:31 -0400

    Crank: First ORM docs pass

--- qcfractal/interface/orm/build_orm.py
+++ qcfractal/interface/orm/build_orm.py
@@ -7,7 +7,33 @@ from .crank_orm import CrankORM
 def build_orm(data, service=None):
     """
     Constructs a Service ORM from incoming JSON data.
-    This data should have a "service" label.
+
+    Parameters
+    ----------
+    data : dict
+        A JSON representation of the service.
+    service : None, optional
+        The name of the service. If blank the service name is pulled from the `data["service"]` field.
+
+    Returns
+    -------
+    ret : a ORM-like object
+        Returns an interface object of the appropriate service.
+
+    Examples
+    --------
+
+    # A partial example of crank metadata
+    >>> data = {
+        "service": "crank",
+        "initial_molecule": "5b7f1fd57b87872d2c5d0a6c",
+        "state": "RUNNING",
+        "id": "5b7f1fd57b87872d2c5d0a6d",
+        ....
+    }
+
+    >>> build_orm(data)
+    Crank(id='5b7f1fd57b87872d2c5d0a6c', state='RUNNING', molecule_id='5b7f1fd57b87872d2c5d0a6c', molecule_name='HOOH')
     """
 
     if ("service" not in data) and (service is None):
--- qcfractal/interface/orm/crank_orm.py
+++ qcfractal/interface/orm/crank_orm.py
@@ -5,7 +5,11 @@ A ORM for Crank
 import json
 
 class CrankORM:
+    """
+    A interface to the raw JSON data of a Crank torsion scan run.
+    """
 
+    # Maps {internal_state : FractalServer state}
     __json_mapper = {
         "_id": "id",
         "_state": "state",
@@ -21,8 +25,21 @@ class CrankORM:
     }
 
     def __init__(self, initial_molecule, **kwargs):
+        """Initializes a CrankORM object, from local data.
 
-        self.initial_molecule = initial_molecule
+        This object may be able to submit jobs to the server in the future.
+
+        *Prototype object, may change in the future.
+
+        Parameters
+        ----------
+        initial_molecule : TYPE
+            Description
+        kwargs:
+            See CrankORM.from_json
+
+        """
+        self._initial_molecule = initial_molecule
 
         # Set kwargs
         for k in self.__json_mapper.keys():
@@ -30,7 +47,27 @@ class CrankORM:
 
     @classmethod
     def from_json(cls, data):
+        """
+        Creates a CrankORM object from FractalServer data.
 
+        Parameters
+        ----------
+        data : dict
+            A JSON blob from FractalServer:
+                - "id": The service id of the blob
+                - "state": The current state of the job ("WAITING", "RUNNING", "FINISHED")
+                - "initial_molecule": The id of the submitted molecule
+                - "crank_meta": The option submitted to the Crank method
+                - "geometric_meta": The options submitted to the Geometric method called by Crank
+                - "qc_meta": The program, options, method, and basis to be run by Geometric.
+                - "final_energies": A dictionary of final energies if the Crank service is finished
+
+        Returns
+        -------
+        crank_obj : CrankORM
+            A CrankORM object from the specified JSON.
+
+        """
         kwargs = {}
         for k, v in CrankORM.__json_mapper.items():
             if v in data:
@@ -44,21 +81,54 @@ class CrankORM:
     def __repr__(self):
         """
         Simplified crank string representation.
+
+        Returns
+        -------
+        ret : str
+            A representation of the current Crank state.
+
+        Examples
+        --------
+
+        >>> repr(crank_obj)
+        Crank(id='5b7f1fd57b87872d2c5d0a6d', state='FINISHED', molecule_id='5b7f1fd57b87872d2c5d0a6c', molecule_name='HOOH')
         """
 
         ret = "Crank("
-        ret += "id={}, ".format(self._id)
-        ret += "state={}, ".format(self._state)
+        ret += "id='{}', ".format(self._id)
+        ret += "state='{}', ".format(self._state)
+        ret += "molecule_id='{}', ".format(self._state)
 
         name = None
-        if self.initial_molecule:
-            name = self.initial_molecule.name()
+        if self._initial_molecule:
+            name = self._initial_molecule.name()
 
-        ret += "initial_molecule={})".format(name)
+        ret += "molecule_name='{}')".format(name)
 
         return ret
 
     def final_energies(self, key=None):
+        """
+        Provides the final optimized energies at each grid point.
+
+        Parameters
+        ----------
+        key : None, optional
+            Returns the final energy at a single grid point.
+
+
+        Returns
+        -------
+        energy : float, dict
+            Returns energies at each grid point in a dictionary or at a
+            single point if a key is specified.
+
+        Examples
+        --------
+
+        >>> crank_obj.final_energies()
+        {(-90,): -148.7641654446243, (180,): -148.76501336993732, (0,): -148.75056290106735, (90,): -148.7641654446148}
+        """
 
         if self._state != "FINISHED":
             raise KeyError("{} has not completed. Unable to show final energies.".format(self))
