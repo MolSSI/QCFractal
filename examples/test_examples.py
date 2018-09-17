901be152e4a4bb8de6c1c1f0131cd6f41ad6c89a
Daniel Smith <malorian@me.com>
2018-09-17 10:23:58 -0400

    Examples: Adds testing to Travis framework

--- examples/test_examples.py
+++ examples/test_examples.py
@@ -2,29 +2,40 @@
 Test the examples
 """
 
-import re
+import os
 import pytest
 import subprocess as sp
 from qcfractal import testing
 
 
+def _run_command(folder, script):
+    # Get the examples director
+    root = os.path.abspath(os.path.dirname(__file__)) 
+    example_path = os.path.join(root, folder)
+    os.chdir(example_path)
+
+    error = False
+    try:
+        output = sp.check_output(["bash", script], shell=False)
+    except sp.CalledProcessError as e:
+        output = e.output
+        error = True
+    
+    os.chdir(root)
+    if error:
+        msg = "Example {} failed. Output as follows\n\n".format(folder)
+        msg += output.decode()
+        raise SystemError(msg)
+
+    return not error
+
+@testing.using_psi4
 @testing.using_fireworks
 @testing.using_unix
-@pytest.mark.slow
-def test_fireworks_example():
+@pytest.mark.example
+def test_fireworks_server_example():
     """Make sure the Fireworks example works as intended"""
-    output = sp.check_output(["bash", "run_fireworks_example.sh"], shell=False)
-    output_lines = output.decode().split('\n')  # Split up the output
-    expected = {
-        'Water Dimer': -1.392710,
-        'Water Dimer Stretch': 0.037144,
-        'Helium Dimer': -0.003148
-    }
-    for index, line in enumerate(output_lines):
-        try:
-            split = re.split(r"([-.0-9]+$)", line)  # Split the line around what we expect
-            expected_value = expected[split[0].strip()]  # use first column as key
-            assert expected_value == float(split[1])  # Check that output is expected value
-        except (KeyError, IndexError):
-            # Skip over lines which are not in the right format
-            pass
+
+    assert _run_command("fireworks_server", "run_fireworks_example.sh")
+
+        
