2a8abf355cd25f73c8f0fec78570ef594182ae36
Daniel Smith <malorian@me.com>
2018-10-28 17:42:46 -0400

    CLI: Refactors to allow for API access

--- qcfractal/cli/qcfractal_manager.py
+++ qcfractal/cli/qcfractal_manager.py
@@ -9,46 +9,58 @@ import tornado.log
 import qcfractal
 from . import cli_utils
 
-parser = argparse.ArgumentParser(description='A CLI for the QCFractal QueueManager.')
-subparsers = parser.add_subparsers(help='QueueManager Backend Type', dest='adapter_type')
-dask_parser = subparsers.add_parser('dask', help='Dask QueueManager')
-fw_parser = subparsers.add_parser('fireworks', help='Fireworks QueueManager')
-
-# Options for Dask
-dask_parser.add_argument("--dask-uri", type=str, help="URI of the dask-server")
-dask_parser.add_argument(
-    "--local-cluster", action="store_true", help="Start a Dask LocalCluster rather than connect to a scheduler")
-dask_parser.add_argument("--local-workers", type=int, default=None, help="The number of workers for the LocalCluster")
-
-# Options for Fireworks
-fw_parser.add_argument("--fw-config", type=str, help="A FWConfig file")
-fw_parser.add_argument("--fw-uri", type=str, help="URI of MongoDB server")
-fw_parser.add_argument(
-    "--fw-name", type=str, default="qcfractal_fireworks_manager", help="The MongoDB Database to use locally")
-
-# FractalClient options
-parser.add_argument("--fractal-uri", type=str, default="localhost:7777", help="FractalServer location to pull from")
-parser.add_argument("-u", "--username", type=str, help="FractalServer username")
-parser.add_argument("-p", "--password", type=str, help="FractalServer password")
-parser.add_argument("--noverify", action="store_true", default=True, help="The logfile prefix to use")
-
-# QueueManager options
-parser.add_argument("--max-tasks", type=int, default=1000, help="Maximum number of tasks to hold at any given time.")
-parser.add_argument("--cluster-name", type=str, default="unknwon", help="The name of the compute cluster to start")
-parser.add_argument("--queue-tag", type=str, help="The queue tag to pull from")
-parser.add_argument("--logfile-prefix", type=str, default=None, help="")
-
-# Additional args
-parser.add_argument("--rapidfire", action="store_true", help="Boot and run jobs until complete")
-parser.add_argument("--config-file", type=str, default=None, help="A configuration file to use")
-
-args = vars(parser.parse_args())
-if args["config_file"] is not None:
-    data = cli_utils.read_config_file(args["config_file"])
-    args = cli_utils.argparse_config_merge(parser, args, data, parser_default=[args["adapter_type"]])
+__all__ = ["main"]
 
 
-def main():
+def parse_args():
+    parser = argparse.ArgumentParser(description='A CLI for the QCFractal QueueManager.')
+    subparsers = parser.add_subparsers(help='QueueManager Backend Type', dest='adapter_type')
+    dask_parser = subparsers.add_parser('dask', help='Dask QueueManager')
+    fw_parser = subparsers.add_parser('fireworks', help='Fireworks QueueManager')
+
+    # Options for Dask
+    dask_parser.add_argument("--dask-uri", type=str, help="URI of the dask-server")
+    dask_parser.add_argument(
+        "--local-cluster", action="store_true", help="Start a Dask LocalCluster rather than connect to a scheduler")
+    dask_parser.add_argument(
+        "--local-workers", type=int, default=None, help="The number of workers for the LocalCluster")
+
+    # Options for Fireworks
+    fw_parser.add_argument("--fw-config", type=str, help="A FWConfig file")
+    fw_parser.add_argument("--fw-uri", type=str, help="URI of MongoDB server")
+    fw_parser.add_argument(
+        "--fw-name", type=str, default="qcfractal_fireworks_manager", help="The MongoDB Database to use locally")
+
+    # FractalClient options
+    parser.add_argument(
+        "--fractal-uri", type=str, default="localhost:7777", help="FractalServer location to pull from")
+    parser.add_argument("-u", "--username", type=str, help="FractalServer username")
+    parser.add_argument("-p", "--password", type=str, help="FractalServer password")
+    parser.add_argument("--noverify", action="store_true", default=True, help="The logfile prefix to use")
+
+    # QueueManager options
+    parser.add_argument(
+        "--max-tasks", type=int, default=1000, help="Maximum number of tasks to hold at any given time.")
+    parser.add_argument("--cluster-name", type=str, default="unknown", help="The name of the compute cluster to start")
+    parser.add_argument("--queue-tag", type=str, help="The queue tag to pull from")
+    parser.add_argument("--logfile-prefix", type=str, default=None, help="")
+
+    # Additional args
+    parser.add_argument("--rapidfire", action="store_true", help="Boot and run jobs until complete")
+    parser.add_argument("--config-file", type=str, default=None, help="A configuration file to use")
+    args = vars(parser.parse_args())
+    if args["config_file"] is not None:
+        data = cli_utils.read_config_file(args["config_file"])
+        args = cli_utils.argparse_config_merge(parser, args, data, parser_default=[args["adapter_type"]])
+
+    return args
+
+
+def main(args=None):
+
+    # Grab CLI args if not present
+    if args is None:
+        args = parse_args()
 
     exit_callbacks = []
 
@@ -108,7 +120,7 @@ def main():
         manager.logger.info("\nDask QueueManager initialized: {}\n".format(str(queue_client)))
     elif args["adapter_type"] == "fireworks":
         manager.logger.info("\nFireworks QueueManager initialized: \n"
-                           "    Host: {}, Name: {}\n".format(queue_client.host, queue_client.name))
+                            "    Host: {}, Name: {}\n".format(queue_client.host, queue_client.name))
 
     # Add exit callbacks
     for cb in exit_callbacks:
@@ -118,6 +130,7 @@ def main():
     if args["rapidfire"]:
         manager.await_results()
     else:
+        # Blocks until keyboard interupt
         manager.start()
 
 
