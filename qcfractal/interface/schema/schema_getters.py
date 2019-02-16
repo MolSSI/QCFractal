fe2c8844000fe185eb0c5b72e2b8d5849f77728f
Daniel Smith <malorian@me.com>
2019-02-16 14:35:34 -0500

    Cleanup: Removes old schema

--- qcfractal/interface/schema/schema_getters.py
+++ qcfractal/interface/schema/schema_getters.py
@@ -2,24 +2,7 @@
 Assists in grabbing the requisite schema
 """
 
-import copy
-
-from .definitions_schema import get_definition
-from .molecule_schema import molecule_schema
-from .options_schema import options_schema
-
-__all__ = [
-    "get_schema", "get_table_indices", "get_schema_keys", "get_hash_fields", "format_result_indices"
-]
-
-_schemas = {}
-
-# Add in molecule
-for req in molecule_schema["required_definitions"]:
-    molecule_schema["definitions"][req] = get_definition(req)
-
-_schemas["molecule"] = molecule_schema
-_schemas["options"] = options_schema
+__all__ = ["get_table_indices", "format_result_indices"]
 
 # Load molecule schema
 
@@ -39,11 +22,6 @@ _table_indices = {
 }  # yapf: disable
 
 
-def get_hash_fields(name):
-    if name not in _schemas:
-        raise KeyError("Schema name {} not found.".format(name))
-    return copy.deepcopy(_schemas[name]["hash_fields"])
-
 
 def get_table_indices(name):
     if name not in _table_indices:
@@ -55,15 +33,3 @@ def format_result_indices(data, program=None):
     if program is None:
         program = data["program"]
     return program, data["molecule"], data["driver"], data["method"], data["basis"], data["options"]
-
-
-def get_schema(name):
-    if name not in _schemas:
-        raise KeyError("Schema name {} not found.".format(name))
-    return copy.deepcopy(_schemas)
-
-
-def get_schema_keys(name):
-    if name not in _schemas:
-        raise KeyError("Schema name {} not found.".format(name))
-    return _schemas[name]["properties"].keys()
