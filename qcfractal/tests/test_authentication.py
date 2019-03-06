912258661668d3ef02612cb7b0970619a0d48b14
Daniel Smith <malorian@me.com>
2019-03-06 13:20:11 -0500

    Security: Slightly more robust security with default level of admin

--- qcfractal/tests/test_authentication.py
+++ qcfractal/tests/test_authentication.py
@@ -52,34 +52,10 @@ def sec_server(request):
 
 
 @pytest.fixture(scope="module")
-def sec_server(request):
-    """
-    Builds a server instance with the event loop running in a thread.
-    """
-
-    # Check mongo
-    testing.check_active_mongo_server()
-
-    storage_name = "qcf_local_server_auth_test"
-
-    with testing.loop_in_thread() as loop:
-
-        # Build server, manually handle IOLoop (no start/stop needed)
-        server = qcfractal.FractalServer(
-            port=testing.find_open_port(), storage_project_name=storage_name, loop=loop, security="local")
-
-        # Clean and re-init the databse
-        server.storage.client.drop_database(server.storage._project_name)
-
-        # Add local users
-        for k, v in _users.items():
-            assert server.storage.add_user(k, _users[k]["pw"], _users[k]["perm"])
-
-        yield server
-
-
-@pytest.fixture(scope="module")
 def sec_server_allow_read(sec_server):
+    """
+    New sec server with read allowed
+    """
 
     yield qcfractal.FractalServer(
         name="qcf_server_allow_read",
@@ -87,7 +63,7 @@ def sec_server_allow_read(sec_server):
         storage_project_name=sec_server.storage.get_project_name(),
         loop=sec_server.loop,
         security="local",
-        allow_read=True, )
+        allow_read=True)
 
 
 ### Tests the compute queue stack
