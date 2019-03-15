68c25c98fea228e4b59f4e73e87ff3e0f7781db5
Daniel Smith <malorian@me.com>
2019-03-15 09:55:22 -0400

    QCPortal: Versioneer after installation fixes

--- qcfractal/interface/__init__.py
+++ qcfractal/interface/__init__.py
@@ -10,3 +10,17 @@ from . import models
 # Add imports here
 from .client import FractalClient
 from .models import Molecule
+
+# We are running inside QCPortal repo
+try:
+    from . import _version
+    versions = _version.get_versions()
+    __version__ = versions["version"]
+    __git_revision__ = versions["full-revisionid"]
+
+# We are running inside QCFractal
+except ImportError:
+    from ..extras import get_information
+    __version__ = "inplace-{}".format(get_information('version'))
+    __git_revision__ = get_information('git_revision')
+    del get_information
