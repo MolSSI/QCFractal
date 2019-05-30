88685ad52fba006439253fa4bf1ae5f77a242bcb
doaa-altarawy <doaa.altarawy@gmail.com>
2019-05-30 16:33:49 -0400

    Alter procedure constraints PK

--- qcfractal/storage_sockets/migrations/drop_procedures.py
+++ qcfractal/storage_sockets/migrations/drop_procedures.py
@@ -10,38 +10,49 @@ sql_uri = "postgresql+psycopg2://qcarchive:mypass@localhost:5432/qcarchivedb"
 
 sql_storage = storage_socket_factory(sql_uri, 'qcarchivedb', db_type='sqlalchemy')
 
-with sql_storage.session_scope() as session:
+with sql_storage.engine.connect() as con:
 
-        procedures = session.query(OptimizationProcedureORM).all()
-        print('Deleteing Opt proc: ', len(procedures))
-        # delete through session to delete correctly from base_result
-        for proc in procedures:
-            session.delete(proc)
+    con.execute('ALTER TABLE optimization_history ' +
+                    'DROP CONSTRAINT optimization_history_pkey;')
 
-        procedures = session.query(TorsionDriveProcedureORM).all()
-        print('Deleteing Torsion proc: ', len(procedures))
-        # delete through session to delete correctly from base_result
-        for proc in procedures:
-            session.delete(proc)
+    con.execute('ALTER TABLE optimization_history '
+                    'ADD CONSTRAINT optimization_history_pkey '
+                    'PRIMARY KEY (torsion_id, opt_id, key, position);')
 
-        procedures = session.query(GridOptimizationProcedureORM).all()
-        print('Deleteing Grid proc: ', len(procedures))
-        # delete through session to delete correctly from base_result
-        for proc in procedures:
-            session.delete(proc)
+# with sql_storage.session_scope() as session:
 
-        session.commit()
+        # procedures = session.query(OptimizationProcedureORM).all()
+        # print('Deleteing Opt proc: ', len(procedures))
+        # # delete through session to delete correctly from base_result
+        # for proc in procedures:
+        #     session.delete(proc)
+        #
+        # procedures = session.query(TorsionDriveProcedureORM).all()
+        # print('Deleteing Torsion proc: ', len(procedures))
+        # # delete through session to delete correctly from base_result
+        # for proc in procedures:
+        #     session.delete(proc)
+        #
+        # procedures = session.query(GridOptimizationProcedureORM).all()
+        # print('Deleteing Grid proc: ', len(procedures))
+        # # delete through session to delete correctly from base_result
+        # for proc in procedures:
+        #     session.delete(proc)
+        #
+        # session.commit()
+        #
+        # # drop tables
+        # torsion_init_mol_association.drop(sql_storage.engine)
+        # OptimizationHistory.__table__.drop(sql_storage.engine)
+        # GridOptimizationAssociation.__table__.drop(sql_storage.engine)
+        # Trajectory.__table__.drop(sql_storage.engine)
+        #
+        # OptimizationProcedureORM.__table__.drop(sql_storage.engine)
+        # TorsionDriveProcedureORM.__table__.drop(sql_storage.engine)
+        # GridOptimizationProcedureORM.__table__.drop(sql_storage.engine)
+        #
+        # ProcedureMap.__table__.drop(sql_storage.engine)
 
-        # drop tables
-        torsion_init_mol_association.drop(sql_storage.engine)
-        OptimizationHistory.__table__.drop(sql_storage.engine)
-        GridOptimizationAssociation.__table__.drop(sql_storage.engine)
-        Trajectory.__table__.drop(sql_storage.engine)
 
-        OptimizationProcedureORM.__table__.drop(sql_storage.engine)
-        TorsionDriveProcedureORM.__table__.drop(sql_storage.engine)
-        GridOptimizationProcedureORM.__table__.drop(sql_storage.engine)
-
-        ProcedureMap.__table__.drop(sql_storage.engine)
 
 
