3f67f6acdbe47b4011084f424799b33f111c805d
Daniel Smith <malorian@me.com>
2019-06-26 13:28:02 -0400

    Config: Global config and regigging

--- qcfractal/config.py
+++ qcfractal/config.py
@@ -9,25 +9,15 @@ from math import ceil
 from pathlib import Path
 
 from typing import List, Optional
-
-import tornado.log
-
-import qcengine as qcng
-import qcfractal
 from pydantic import BaseModel, BaseSettings, validator, Schema
 
-from . import cli_utils
 
-__all__ = ["main"]
-
-QCA_FOLDER = Path(os.path.expanduser("~/.qca/"))
-QCF_CONFIG_FILE = QCA_FOLDER / "qcfractal_config.yaml"
-QCF_DATABASE_FOLDER = QCA_FOLDER / "qcfractal_data"
 
 class SettingsCommonConfig:
     env_prefix = "QCF_"
     case_insensitive = True
     extra = "forbid"
+
 
 class ConfigSettings(BaseSettings):
 
@@ -62,13 +52,14 @@ class DatabaseSettings(ConfigSettings):
     )
     username: str = Schema("qcfractal_user", description="The postgres username to default to.")
     password: str = Schema(None, description="The postgres password for the give user.")
-    directory: str = Schema("~/.qca/qcfractal_data/",
-                               description="The physical location of the QCFractal instance data.")
+    directory: str = Schema(
+        None, description="The physical location of the QCFractal instance data, defaults to the root folder.")
     default_database: str = Schema("qcfractal_default", description="The default database to connect to.")
     logfile: str = Schema("qcfractal_postgres.log", description="The logfile to write postgres logs.")
 
     class Config(SettingsCommonConfig):
         pass
+
 
 class FractalServerSettings(ConfigSettings):
     """
@@ -80,17 +71,32 @@ class FractalServerSettings(ConfigSettings):
 
     query_limit: int = Schema(1000, description="The maximum number of records to return per query.")
     logfile: str = Schema("qcfractal_server.log", description="The logfile to write server logs.")
-    max_active_services: int = Schema(20,
-                               description="The maximum number of concurrent active services.")
-    heartbeat_frequency: int = Schema(1800, description="The frequency (in seconds) to check the heartbeat of workers.")
+    max_active_services: int = Schema(20, description="The maximum number of concurrent active services.")
+    heartbeat_frequency: int = Schema(1800,
+                                      description="The frequency (in seconds) to check the heartbeat of workers.")
 
     class Config(SettingsCommonConfig):
         pass
+
 
 class FractalConfig(ConfigSettings):
 
+    base_folder: str = Schema(os.path.expanduser("~/.qca/qcfractal"),
+                              description="The QCFractal base instance to attach to.")
     database: DatabaseSettings = DatabaseSettings()
-    fractal:  FractalServerSettings = FractalServerSettings()
+    fractal: FractalServerSettings = FractalServerSettings()
 
     class Config(SettingsCommonConfig):
         pass
+
+    def base_path(self):
+        return Path(self.base_folder)
+
+    def config_file_path(self):
+        return self.base_path() / "qcfractal_config.yaml"
+
+    def database_path(self):
+        if self.database.directory is None:
+            return self.base_path() / "qcfractal_data"
+        else:
+            return Path(os.path.expanduser(self.database.directory))
