37d0b92193d3e6571ec27ba7e0e0eed172822f26
Daniel Smith <malorian@me.com>
2019-07-18 16:01:35 -0400

    CI: Update pydantic pins to >=0.30.1

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ if __name__ == "__main__":
             'cryptography',
             'numpy>=1.7',
             'pandas',
-            'pydantic>=0.20,<0.30',
+            'pydantic>=0.30.1',
             'pymongo>=3.0',
             'requests',
             'tornado',
