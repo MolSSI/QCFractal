041d10cbd656eddc3701d7739d06184aa3430b14
Daniel Smith <malorian@me.com>
2019-09-19 15:26:49 -0400

    Collections: Provides a upgrade path for new dataset entry pkey constraint

--- qcfractal/storage_sockets/models/collections_models.py
+++ qcfractal/storage_sockets/models/collections_models.py
@@ -78,7 +78,7 @@ class DatasetEntryORM(Base):
 
     dataset_id = Column(Integer, ForeignKey('dataset.id', ondelete='cascade'), primary_key=True)
     #TODO: check the cascase_delete with molecule
-    molecule_id = Column(Integer, ForeignKey('molecule.id'))
+    molecule_id = Column(Integer, ForeignKey('molecule.id'), nullable=False)
 
     name = Column(String, nullable=False, primary_key=True)
     comment = Column(String)
