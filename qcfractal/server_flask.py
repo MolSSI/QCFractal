c7a5aa2048065d21231a09005bc3b5ed938247c0
daltarawy <daltarawy@vt.edu>
2020-09-23 18:37:32 -0400

    Add handle_collection

--- qcfractal/server_flask.py
+++ qcfractal/server_flask.py
@@ -215,6 +215,101 @@ def get_kvstore():
 
     return data
 
+
+@app.route('/collection/<int:collection_id>/<string:view_function>', methods=['GET','POST','DELETE'])
+@jwt_required
+def handle_collection(collection_id: int, view_function: str):
+
+    if request.method == 'GET':
+        # List collections
+        if (collection_id is None) and (view_function is None):
+            body_model, response_model = rest_model("collection", "get")
+            body = parse_bodymodel(body_model)
+
+            cols = storage.get_collections(
+                **body.data.dict(), include=body.meta.include, exclude=body.meta.exclude
+            )
+            response = response_model(**cols)
+
+        # Get specific collection
+        elif (collection_id is not None) and (view_function is None):
+            body_model, response_model = rest_model("collection", "get")
+
+            body = parse_bodymodel(body_model)
+            cols = storage.get_collections(
+                **body.data.dict(), col_id=int(collection_id), include=body.meta.include, exclude=body.meta.exclude
+            )
+            response = response_model(**cols)
+
+        # View-backed function on collection
+        elif (collection_id is not None) and (view_function is not None):
+            body_model, response_model = rest_model(f"collection/{collection_id}/{view_function}", "get")
+            body = parse_bodymodel(body_model)
+            if view_handler is None:
+                meta = {
+                    "success": False,
+                    "error_description": "Server does not support collection views.",
+                    "errors": [],
+                    "msgpacked_cols": [],
+                }
+                response = response_model(meta=meta, data=None)
+                if not isinstance(response, (str, bytes)):
+                    data = serialize(response, encoding)
+
+                return data
+
+            result = view_handler.handle_request(collection_id, view_function, body.data.dict())
+            response = response_model(**result)
+
+        # Unreachable?
+        else:
+            body_model, response_model = rest_model("collection", "get")
+            meta = add_metadata_template()
+            meta["success"] = False
+            meta["error_description"] = "GET request for view with no collection ID not understood."
+            response = response_model(meta=meta, data=None)
+
+        if not isinstance(response, (str, bytes)):
+            data = serialize(response, encoding)
+
+        return data
+
+    elif request.method == 'POST':
+        body_model, response_model = rest_model("collection", "post")
+        body = parse_bodymodel(body_model)
+
+        # POST requests not supported for anything other than "/collection"
+        if collection_id is not None or view_function is not None:
+            meta = add_metadata_template()
+            meta["success"] = False
+            meta["error_description"] = "POST requests not supported for sub-resources of /collection"
+            response = response_model(meta=meta, data=None)
+            if not isinstance(response, (str, bytes)):
+                data = serialize(response, encoding)
+
+            return data
+
+        ret = storage.add_collection(body.data.dict(), overwrite=body.meta.overwrite)
+        response = response_model(**ret)
+
+        if not isinstance(response, (str, bytes)):
+            data = serialize(response, encoding)
+
+        return data
+
+    elif request.method == 'DELETE':
+        body_model, response_model = rest_model(f"collection/{collection_id}", "delete")
+        ret = storage.del_collection(col_id=collection_id)
+        if ret == 0:
+            return jsonify(message="Collection does not exist."), 404
+        else:
+            response = response_model(meta={"success": True, "errors": [], "error_description": False})
+
+        if not isinstance(response, (str, bytes)):
+            data = serialize(response, encoding)
+
+        return data
+
 # @app.route('/retrieve_password/<string:email>', methods=['GET'])
 # def retrieve_password(email: str):
 #     user = User.query.filter_by(email=email).first()
