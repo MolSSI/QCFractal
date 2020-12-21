ea5f941a2270790697f057c5f5497f1ea1bb1189
doaa-altarawy <doaa.altarawy@gmail.com>
2020-12-21 18:03:50 -0500

    tests_storage pass, add lib responses instead on requets_mock

--- qcfractal/storage_sockets/storage_socket.py
+++ qcfractal/storage_sockets/storage_socket.py
@@ -1,9 +1,10 @@
 """
 A factory for Database sockets
 """
+from . import sqlalchemy_socket
 
 
-def storage_socket_factory(uri, project_name="", logger=None, db_type=None, **kwargs):
+def storage_socket_factory(uri, project_name="", logger=None, **kwargs):
     """
     Factory for generating storage sockets. Spins up a given storage layer on request given common inputs.
 
@@ -17,8 +18,6 @@ def storage_socket_factory(uri, project_name="", logger=None, db_type=None, **kw
         Name of the project
     logger : logging.Logger, Optional, Default: None
         Specific logger to report to
-    db_type : string, Optional, Default: 'sqlalchemy'
-        socket type, 'sqlalchemy'
     **kwargs
         Additional keyword arguments to pass to the storage constructor
 
@@ -27,16 +26,4 @@ def storage_socket_factory(uri, project_name="", logger=None, db_type=None, **kw
 
     """
 
-    if db_type is None:
-        # try to find db_type from uri
-        if uri.startswith("postgresql"):
-            db_type = "sqlalchemy"
-        else:
-            raise TypeError("Unknown DB type, uri: {}".format(uri))
-
-    if db_type == "sqlalchemy":
-        from . import sqlalchemy_socket
-
-        return sqlalchemy_socket.SQLAlchemySocket(uri, project=project_name, logger=logger, **kwargs)
-    else:
-        raise KeyError("DBType {} not understood".format(db_type))
+    return sqlalchemy_socket.SQLAlchemySocket(uri, project=project_name, logger=logger, **kwargs)
