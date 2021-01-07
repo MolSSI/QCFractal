9444c5eb87dfdf7bc3698b0c34ac27dad0e263b2
doaa-altarawy <doaa.altarawy@gmail.com>
2021-01-07 16:10:38 -0500

    Add flask SSL, add manager executor

--- qcfractal/app/__init__.py
+++ qcfractal/app/__init__.py
@@ -19,7 +19,7 @@ def create_app(config_name="default", **objects):
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
 
-    #     cors.init_app(app)
+    # cors.init_app(app)
     jwt.init_app(app)
 
     ### Add Fractal config, like storage, logger, etc
@@ -32,12 +32,6 @@ def create_app(config_name="default", **objects):
     app.config.public_information = objects["public_information"]
     app.config.JWT_ENABLED = objects["JWT_ENABLED"]
     app.config.ALLOW_READ = objects["ALLOW_READ"]
-
-    # TODO: not tested
-    if app.config["SSL_REDIRECT"]:
-        from flask_sslify import SSLify
-
-        sslify = SSLify(app)
 
     logger.debug("Adding blueprints..")
 
