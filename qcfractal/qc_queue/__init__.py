39ee0980c98cdb69abc7aa88efa2096162bca9dc
Benjamin Pritchard <ben@bennyp.org>
2021-02-11 17:02:00 -0500

    remove unused code

--- qcfractal/qc_queue/__init__.py
+++ qcfractal/qc_queue/__init__.py
@@ -3,5 +3,4 @@ Initializer for the queue_handler folder
 """
 
 from .adapters import build_queue_adapter
-from .handlers import QueueManagerHandler, ServiceQueueHandler, TaskQueueHandler, ComputeManagerHandler
 from .managers import QueueManager
