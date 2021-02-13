ab4678962c7b423b94d034b668830752957bf031
Benjamin Pritchard <ben@bennyp.org>
2021-02-13 14:52:14 -0500

    Slight cleanup of a few files

--- qcfractal/port_util.py
+++ qcfractal/port_util.py
@@ -6,13 +6,33 @@ import socket
 
 
 def find_port() -> int:
+    """
+    Finds an open port that we can bind to
+    """
+
     sock = socket.socket()
     sock.bind(("localhost", 0))
-    host, port = sock.getsockname()
+    _, port = sock.getsockname()
     return port
 
 
-def is_port_open(ip: str, port: int) -> bool:
+def is_port_inuse(ip: str, port: int) -> bool:
+    """
+    Determine if an ip/port is being used or not
+
+    Parameters
+    ----------
+    ip: str
+        The host IP address to check
+    port: int
+        The port on the IP address to check
+
+
+    Returns
+    -------
+    bool
+        True if the port is currently in use, False otherwise
+    """
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.settimeout(1)
         try:
