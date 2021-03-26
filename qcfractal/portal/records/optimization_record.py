e19b923c8dde7f64e51bf40c8ddcae3b782f8ba3
David Dotson <dotsdl@gmail.com>
2021-03-26 11:39:31 -0700

    Adding our own Record objects, with composition of pydantic data model

---
+++ qcfractal/portal/records/optimization_record.py
@@ -0,0 +1,25 @@
+from typing import Any
+
+from ...interface.models import OptimizationRecord as _OptimizationRecord
+
+
+class OptimizationRecord:
+    """
+    User-facing API for accessing data for a single optimization.
+
+    """
+    _DataModel = _OptimizationRecord
+
+    def __init__(self, **kwargs: Any):
+        """
+
+        Parameters
+        ----------
+        **kwargs : Dict[str, Any]
+            Additional keywords passed to the OptimizationRecord and the initial data constructor.
+        """
+        # Create the data model
+        self._data = self._DataModel(**kwargs)
+
+
+register_record(OptimizationRecord)
