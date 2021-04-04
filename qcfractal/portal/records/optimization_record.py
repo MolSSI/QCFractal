b0c9a994355734d5296754d3a1bdc26f89244504
David Dotson <dotsdl@gmail.com>
2021-04-03 23:00:47 -0700

    to_json, from_json, to_dict, from_dict methods on Records
    Also, now record factory approach mirrors that for collections.

--- qcfractal/portal/records/optimization_record.py
+++ qcfractal/portal/records/optimization_record.py
@@ -1,15 +1,17 @@
 from typing import Any
 
 from ...interface.models import OptimizationRecord as _OptimizationRecord
+from .record import Record
 from .record_utils import register_record
 
 
-class OptimizationRecord:
+class OptimizationRecord(Record):
     """
     User-facing API for accessing data for a single optimization.
 
     """
     _DataModel = _OptimizationRecord
+    _type = "optimization"
 
     def __init__(self, **kwargs: Any):
         """
@@ -19,8 +21,13 @@ class OptimizationRecord:
         **kwargs : Dict[str, Any]
             Additional keywords passed to the OptimizationRecord and the initial data constructor.
         """
-        # Create the data model
         self._data = self._DataModel(**kwargs)
+
+    @property
+    def status(self):
+        return self._data.status
+
+    # TODO: add in optimization-specific methods
 
 
 register_record(OptimizationRecord)
--- qcfractal/portal/records/record_utils.py
+++ qcfractal/portal/records/record_utils.py
@@ -14,19 +14,17 @@ def register_record(record: "RecordBase") -> None:
 
     """
 
-    class_name = record.__name__.lower()
-    __registered_records[class_name] = record
+    record_type = record._type
+    __registered_records[record_type] = record
 
 
-def record_factory(data: Dict[str, Any], client: "PortalClient" = None) -> "RecordBase":
+def record_factory(data: Dict[str, Any]) -> "RecordBase":
     """Returns a Record object from data deserialized from JSON.
 
     Parameters
     ----------
     data : Dict[str, Any]
         The JSON-serializable dict to create a new class from.
-    client : PortalClient, optional
-        A PortalClient connected to a server.
 
     Returns
     -------
@@ -41,7 +39,7 @@ def record_factory(data: Dict[str, Any], client: "PortalClient" = None) -> "Reco
         raise KeyError("Attempted to create Record of unknown type '{}'.".format(data["procedure"]))
 
     # TODO: return here after fixing `from_json`, `to_json` to be less ambiguous
-    return __registered_records[data["procedure"].lower()].from_json(data, client=client)
+    return __registered_records[data["procedure"].lower()].from_dict(data)
 
 
 def record_name_map() -> Dict[str, str]:
@@ -54,56 +52,3 @@ def record_name_map() -> Dict[str, str]:
         Map of {'internal': 'user fiendly name'}
     """
     return {k: v.__name__ for k, v in __registered_records.items()}
-
-
-#def build_procedure(
-#    data: Dict[str, Any], procedure: Optional[str] = None, client: Optional["FractalClient"] = None
-#) -> "BaseRecord":
-#    """
-#    Constructs a Service ORM from incoming JSON data.
-#
-#    Parameters
-#    ----------
-#    data : Dict[str, Any]
-#        A JSON representation of the procedure.
-#    procedure : Optional[str], optional
-#        The name of the procedure. If blank the procedure name is pulled from the `data["procedure"]` field.
-#    client : Optional['FractalClient'], optional
-#        A FractalClient connected to a server.
-#
-#    Returns
-#    -------
-#    ret : BaseRecord
-#        Returns an interface object of the appropriate procedure.
-#
-#    Examples
-#    --------
-#
-#    # A partial example of torsiondrive metadata
-#    >>> data = {
-#        "procedure": "torsiondrive",
-#        "initial_molecule": "5b7f1fd57b87872d2c5d0a6c",
-#        "state": "RUNNING",
-#        "id": "5b7f1fd57b87872d2c5d0a6d",
-#        ....
-#    }
-#
-#    >>> build_orm(data)
-#    TorsionDriveRecord(id='5b7f1fd57b87872d2c5d0a6c', state='RUNNING', molecule_id='5b7f1fd57b87872d2c5d0a6c', molecule_name='HOOH')
-#    """
-#
-#    if ("procedure" not in data) and (procedure is None):
-#        raise KeyError("There is not a procedure tag and procedure is none. Unable to determine procedure type")
-#
-#    # import json
-#    # print(json.dumps(data, indent=2))
-#    if data["procedure"].lower() == "single":
-#        return ResultRecord(**data, client=client)
-#    elif data["procedure"].lower() == "torsiondrive":
-#        return TorsionDriveRecord(**data, client=client)
-#    elif data["procedure"].lower() == "gridoptimization":
-#        return GridOptimizationRecord(**data, client=client)
-#    elif data["procedure"].lower() == "optimization":
-#        return OptimizationRecord(**data, client=client)
-#    else:
-#        raise KeyError("Service names {} not recognized.".format(data["procedure"]))
