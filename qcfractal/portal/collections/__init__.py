e5ef080428ec65ea98578bad84875deb7305d9ea
David Dotson <dotsdl@gmail.com>
2021-08-27 10:54:55 -0700

    Added all collections to registration; working on Dataset._query

--- qcfractal/portal/collections/__init__.py
+++ qcfractal/portal/collections/__init__.py
@@ -1,3 +1,7 @@
 from .collection_utils import collection_factory, collections_name_map, register_collection
+from .collection import Collection
 from .dataset import Dataset
+from .reaction_dataset import ReactionDataset
 from .optimization_dataset import OptimizationDataset
+from .torsiondrive_dataset import TorsionDriveDataset
+from .gridoptimization_dataset import GridOptimizationDataset
