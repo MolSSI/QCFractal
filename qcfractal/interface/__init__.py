baf0208149313beaf184af885c076b4d71df0b28
Benjamin Pritchard <ben@bennyp.org>
2021-09-19 11:14:06 -0400

    Change version handling for qcportal

--- qcfractal/interface/__init__.py
+++ qcfractal/interface/__init__.py
@@ -7,19 +7,15 @@ DQM Client base folder
 # Other packages that we import later will need it
 ###################################################
 
-# We are running inside QCPortal repo
-try:
-    # The _version file exists only in the QCPortal package
-    from ._version import get_versions
+# Always use the _version module in this directory. It should
+# always match the one a directory above, so no need to differentiate.
+from ._version import get_versions
 
-    versions = get_versions()
-    __version__ = versions["version"]
-    __git_revision__ = versions["full-revisionid"]
-    del get_versions, versions
+versions = get_versions()
+__version__ = versions["version"]
+__git_revision__ = versions["full-revisionid"]
+del get_versions, versions
 
-# We are running inside QCFractal
-except ImportError:
-    from .. import __version__, __git_revision__
 
 from . import collections, data, models, util
 
