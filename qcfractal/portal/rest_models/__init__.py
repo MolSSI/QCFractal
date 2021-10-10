37ab51245a4cc750b10a9ef35ba40ea76e189d21
Benjamin Pritchard <ben@bennyp.org>
2021-10-09 20:46:04 -0400

    Add DeleteParameters to REST models

--- qcfractal/portal/rest_models/__init__.py
+++ qcfractal/portal/rest_models/__init__.py
@@ -1,1 +1,1 @@
-from .common import GetParameters, SimpleGetParameters
+from .common import GetParameters, SimpleGetParameters, DeleteParameters
--- qcfractal/portal/rest_models/common.py
+++ qcfractal/portal/rest_models/common.py
@@ -1,9 +1,14 @@
 from typing import Optional, List
+
 from pydantic import BaseModel, validator
 
 
+class DeleteParameters(BaseModel):
+    id: Optional[List[int]] = None
+
+
 class SimpleGetParameters(BaseModel):
-    id: Optional[List[str]] = None
+    id: Optional[List[int]] = None
     missing_ok: Optional[bool] = False
 
     @validator("missing_ok", pre=True)
@@ -16,6 +21,10 @@ class SimpleGetParameters(BaseModel):
 
 
 class GetParameters(BaseModel):
+    """
+    Common query parameters to get functions
+    """
+
     id: Optional[List[str]] = None
     include: Optional[List[str]] = None
     exclude: Optional[List[str]] = None
