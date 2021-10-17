4bf6df435f65a21c907a215a93f8674bc7a9e17f
Benjamin Pritchard <ben@bennyp.org>
2021-10-17 10:24:40 -0400

    Move interface tests to portal

--- qcfractal/portal/components/outputstore/test_models.py
+++ qcfractal/portal/components/outputstore/test_models.py
@@ -1,22 +1,21 @@
 import pytest
 
-import qcfractal.portal.components.outputstore.models
-from . import portal
+from qcfractal.portal.components.outputstore import OutputStore
 
 
-def test_kvstore_model_fail():
+def test_models_outputstore_fail():
     # data is a string, but compression is not none
     with pytest.raises(ValueError, match=r"Compression is set, but input is a"):
-        qcfractal.portal.components.outputstore.models.OutputStore(**{"data": "123", "compression": "bzip2"})
+        OutputStore(**{"data": "123", "compression": "bzip2"})
 
     # data is a dict, but compression is not none
     with pytest.raises(ValueError, match=r"Compression is set, but input is a"):
-        qcfractal.portal.components.outputstore.models.OutputStore(**{"data": {"123": 123}, "compression": "bzip2"})
+        OutputStore(**{"data": {"123": 123}, "compression": "bzip2"})
 
     # data is a string, but compression level is not 0
     with pytest.raises(ValueError, match=r"Compression level is set, but input is a"):
-        qcfractal.portal.components.outputstore.models.OutputStore(**{"data": "123", "compression_level": 1})
+        OutputStore(**{"data": "123", "compression_level": 1})
 
     # data is a string, but compression level is not 0
     with pytest.raises(ValueError, match=r"Compression level is set, but input is a"):
-        qcfractal.portal.components.outputstore.models.OutputStore(**{"data": {"123": 123}, "compression_level": 1})
+        OutputStore(**{"data": {"123": 123}, "compression_level": 1})
