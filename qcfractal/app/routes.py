22905b82f20f06effc76a274d65ff888c314e138
Benjamin Pritchard <ben@bennyp.org>
2021-10-21 20:42:27 -0400

    Move policyuniverse again

--- qcfractal/app/routes.py
+++ qcfractal/app/routes.py
@@ -20,8 +20,7 @@ from werkzeug.exceptions import BadRequest, InternalServerError, HTTPException, 
 from qcfractal.app import main, storage_socket
 from qcfractal.app.helpers import _valid_encodings, SerializedResponse
 from qcfractal.exceptions import UserReportableError, AuthenticationFailure
-from qcfractal.portal.components.permissions.models import UserInfo
-from qcfractal.components.permissions.policyuniverse import Policy
+from qcfractal.app.policyuniverse import Policy
 from qcfractal.portal.serialization import deserialize, serialize
 
 _read_permissions = {}
