f034ad0f70e7d21d23cc6d3e04792989b1e647e3
Benjamin Pritchard <ben@bennyp.org>
2021-11-04 10:50:53 -0400

    Make github workflow for core tests

--- setup.py
+++ setup.py
@@ -25,30 +25,28 @@ if __name__ == "__main__":
         python_requires=">=3.7",
         install_requires=[
             # Core dependencies
-            "numpy >=1.17",
+            "numpy",
             "msgpack",
             "flask",
             "flask_jwt_extended",
             "gunicorn",
-            "requests",
             "pyyaml",
             "pydantic",
             "pyarrow",
             "bcrypt",
-            "cryptography",
-            # Storage dependencies
             "sqlalchemy >=1.4",
             "alembic",
             "psycopg2",
+            "qcelemental>=0.23",
+            "geoip2",
             # QCPortal dependencies
+            "requests",
             "tqdm",
             "plotly",
             "pandas",
             "tabulate",
             "h5py",
-            # QCArchive depends
-            "qcengine==0.22",
-            "qcelemental==0.24",
+            "qcengine>=0.20",
         ],
         entry_points={
             "console_scripts": [
