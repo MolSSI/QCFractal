7d4cc28f1a5c0925255eea898ee230a1123ea625
Benjamin Pritchard <ben@bennyp.org>
2021-12-17 15:51:14 -0500

    Some cleanup/renaming of service stuff

--- qcfractal/components/services/db_models.py
+++ qcfractal/components/services/db_models.py
@@ -3,18 +3,17 @@ import datetime
 from sqlalchemy import Column, Integer, ForeignKey, JSON, String, DateTime, Index, UniqueConstraint
 from sqlalchemy.orm import relationship
 
-from qcfractal.portal.records import PriorityEnum
-from qcfractal.db_socket import BaseORM, PlainMsgpackExt
 from qcfractal.components.records.db_models import BaseRecordORM
+from qcfractal.db_socket import BaseORM, PlainMsgpackExt
 
 
-class ServiceQueueTasks(BaseORM):
+class ServiceQueueTasksORM(BaseORM):
     __tablename__ = "service_queue_tasks"
 
     service_id = Column(Integer, ForeignKey("service_queue.id", ondelete="cascade"), primary_key=True)
-    record_id = Column(Integer, ForeignKey(BaseRecordORM.id, ondelete="cascade"), primary_key=True)
+    record_id = Column(Integer, ForeignKey(BaseRecordORM.id), primary_key=True)
 
-    record = relationship("BaseRecordORM", lazy="selectin")
+    record = relationship("BaseRecordORM")
     extras = Column(JSON)
 
 
@@ -23,17 +22,17 @@ class ServiceQueueORM(BaseORM):
     __tablename__ = "service_queue"
 
     id = Column(Integer, primary_key=True)
-    tag = Column(String, default=None)
 
     record_id = Column(Integer, ForeignKey(BaseRecordORM.id))
     record = relationship(BaseRecordORM, back_populates="service", uselist=False)
 
-    priority = Column(Integer, default=int(PriorityEnum.normal))
-    created_on = Column(DateTime, default=datetime.datetime.utcnow)
+    tag = Column(String)
+    priority = Column(Integer, nullable=False)
+    created_on = Column(DateTime, default=datetime.datetime.utcnow, nullable=False)
 
     service_state = Column(PlainMsgpackExt)
 
-    tasks = relationship(ServiceQueueTasks, lazy="joined", cascade="all, delete-orphan")
+    tasks = relationship(ServiceQueueTasksORM, lazy="selectin")
 
     __table_args__ = (
         UniqueConstraint("record_id", name="ux_service_queue_record_id"),
