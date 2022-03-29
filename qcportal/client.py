67e7a7eb551ca47b8b6bddc238d89c8af3aae501
Benjamin Pritchard <ben@bennyp.org>
2022-03-29 11:44:44 -0400

    Update urls in client

--- qcportal/client.py
+++ qcportal/client.py
@@ -93,7 +93,7 @@ from .utils import make_list, make_str
 class PortalClient(PortalClientBase):
     def __init__(
         self,
-        address: str = "api.qcarchive.molssi.org:443",
+        address: str = "https://api.qcarchive.molssi.org",
         username: Optional[str] = None,
         password: Optional[str] = None,
         verify: bool = True,
--- qcportal/manager_client.py
+++ qcportal/manager_client.py
@@ -16,7 +16,7 @@ class ManagerClient(PortalClientBase):
     def __init__(
         self,
         name_data: ManagerName,
-        address: str = "api.qcarchive.molssi.org:443",
+        address: str = "https://api.qcarchive.molssi.org",
         username: Optional[str] = None,
         password: Optional[str] = None,
         verify: bool = True,
