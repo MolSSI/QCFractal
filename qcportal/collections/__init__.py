fb85717e2085484c501588f9d044ed658b67249e
Benjamin Pritchard <ben@bennyp.org>
2022-04-06 14:53:19 -0400

    Remove old collections code

--- qcportal/collections/__init__.py
+++
@@ -1,7 +0,0 @@
-from .collection import Collection
-from .collection_utils import collection_factory, collections_name_map, register_collection
-from .dataset import Dataset
-from .gridoptimization_dataset import GridOptimizationDataset
-from .optimization_dataset import OptimizationDataset
-from .reaction_dataset import ReactionDataset
-from .torsiondrive_dataset import TorsionDriveDataset
