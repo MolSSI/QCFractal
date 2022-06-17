dc22f037552859db080359c18974205c9b6cbfb8
Benjamin Pritchard <ben@bennyp.org>
2022-06-17 11:45:06 -0400

    Update optimization deletion tests

--- qcfractal/components/records/optimization/test_client.py
+++ qcfractal/components/records/optimization/test_client.py
@@ -7,7 +7,6 @@ import pytest
 
 from qcfractal.db_socket import SQLAlchemySocket
 from qcfractaltesting import load_molecule_data
-from qcportal.managers import ManagerName
 from qcportal.records import RecordStatusEnum, PriorityEnum
 from qcportal.records.optimization import (
     OptimizationSpecification,
@@ -17,6 +16,7 @@ from qcportal.records.singlepoint import QCSpecification
 if TYPE_CHECKING:
     from qcfractal.db_socket import SQLAlchemySocket
     from qcportal import PortalClient
+    from qcportal.managers import ManagerName
 
 from qcfractal.components.records.optimization.testing_helpers import (
     compare_optimization_specs,
@@ -129,6 +129,17 @@ def test_optimization_client_delete(
     rec = storage_socket.records.optimization.get([opt_id], include=["trajectory"])
     child_ids = [x["singlepoint_id"] for x in rec[0]["trajectory"]]
 
+    meta = snowflake_client.delete_records(opt_id, soft_delete=True, delete_children=False)
+    assert meta.success
+    assert meta.deleted_idx == [0]
+    assert meta.n_children_deleted == 0
+
+    child_recs = snowflake_client.get_records(child_ids, missing_ok=True)
+    assert all(x.status == RecordStatusEnum.complete for x in child_recs)
+
+    # Undo what we just did
+    snowflake_client.undelete_records(opt_id)
+
     meta = snowflake_client.delete_records(opt_id, soft_delete=True, delete_children=True)
     assert meta.success
     assert meta.deleted_idx == [0]
@@ -147,6 +158,30 @@ def test_optimization_client_delete(
 
     child_recs = snowflake_client.get_records(child_ids, missing_ok=True)
     assert all(x is None for x in child_recs)
+
+    # DB should be pretty empty now
+    query_res = snowflake_client.query_records()
+    assert query_res.current_meta.n_found == 0
+
+
+def test_optimization_client_harddelete_nochildren(
+    snowflake_client: PortalClient, storage_socket: SQLAlchemySocket, activated_manager_name: ManagerName
+):
+    opt_id = run_test_data(storage_socket, activated_manager_name, "psi4_benzene_opt")
+
+    rec = storage_socket.records.optimization.get([opt_id], include=["trajectory"])
+    child_ids = [x["singlepoint_id"] for x in rec[0]["trajectory"]]
+
+    meta = snowflake_client.delete_records(opt_id, soft_delete=False, delete_children=False)
+    assert meta.success
+    assert meta.deleted_idx == [0]
+    assert meta.n_children_deleted == 0
+
+    recs = snowflake_client.get_optimizations(opt_id, missing_ok=True)
+    assert recs is None
+
+    child_recs = snowflake_client.get_records(child_ids, missing_ok=True)
+    assert all(x is not None for x in child_recs)
 
 
 @pytest.mark.parametrize("opt_file", ["psi4_benzene_opt", "psi4_methane_opt_sometraj"])
