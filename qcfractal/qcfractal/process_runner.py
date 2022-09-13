520e9157e4eb69975db7f7a918da05e6f90a2a41
Benjamin Pritchard <ben@bennyp.org>
2022-09-13 10:40:13 -0400

    Remove unused InterruptableSleep class

--- qcfractal/qcfractal/process_runner.py
+++ qcfractal/qcfractal/process_runner.py
@@ -13,43 +13,6 @@ import weakref
 from typing import Union, Any
 
 
-class SleepInterrupted(BaseException):
-    """
-    Exception class used to signal that an InterruptableSleep was interrupted
-
-    This (like KeyboardInterrupt) derives from BaseException to prevent
-    it from being handled with "except Exception".
-    """
-
-    pass
-
-
-class InterruptableSleep:
-    """
-    A class for sleeping, but interruptable
-
-    This class uses threading Events to wake up from a sleep before the entire sleep
-    duration has run. If the sleep is interrupted, then an SleepInterrupted exception is raised.
-
-    This class is a functor, so an instance can be passed as the delay function to a python
-    sched.scheduler
-    """
-
-    def __init__(self):
-        self._event = threading.Event()
-
-    def __call__(self, delay: float):
-        interrupted = self._event.wait(delay)
-        if interrupted:
-            raise SleepInterrupted()
-
-    def interrupt(self):
-        self._event.set()
-
-    def clear(self):
-        self._event.clear()
-
-
 class ProcessBase(abc.ABC):
     """
     A class to define functionality to be run in another process
