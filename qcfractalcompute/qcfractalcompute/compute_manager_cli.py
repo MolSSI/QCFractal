26e254c0865cac648510e410687e2560a7f457f1
Benjamin Pritchard <ben@bennyp.org>
2023-05-03 10:56:03 -0400

    New compute manager respects logfile/level

--- qcfractalcompute/qcfractalcompute/compute_manager_cli.py
+++ qcfractalcompute/qcfractalcompute/compute_manager_cli.py
@@ -5,6 +5,7 @@ A command line interface to the qcfractal server.
 import argparse
 import logging
 import signal
+import sys
 
 from . import __version__
 from .compute_manager import ComputeManager
@@ -13,18 +14,39 @@ from .config import read_configuration
 
 def main():
 
-    logging.basicConfig()
-
     parser = argparse.ArgumentParser(description="A CLI for a QCFractal QueueManager")
     parser.add_argument("--version", action="version", version=f"{__version__}")
+    parser.add_argument("--verbose", action="store_true", help="Print verbose output")
     parser.add_argument("--config", type=str, default=None)
 
     args = parser.parse_args()
+
+    # Universal formatting for logs
+    formatter = logging.Formatter("[%(asctime)s] %(levelname)8s: %(name)s: %(message)s", "%Y-%m-%d %H:%M:%S %Z")
+
+    # Set up a log handler that is used before the logfile is set up
+    log_handler = logging.StreamHandler(sys.stdout)
+    log_handler.setFormatter(formatter)
+
+    # If the user wants verbose output (particularly about startup of all the commands), then set logging level
+    # to be DEBUG
+    if args.verbose:
+        logging.basicConfig(level="DEBUG", handlers=[log_handler], format="%(levelname)s: %(name)s: %(message)s")
+    else:
+        logging.basicConfig(level="INFO", handlers=[log_handler], format="%(levelname)s: %(message)s")
 
     manager_config = read_configuration([args.config])
 
     # Adjust the logging level to what was in the config
     logging.getLogger().setLevel(manager_config.loglevel)
+
+    if manager_config.logfile:
+        print("*" * 10)
+        print(f"Logging to file {manager_config.logfile}, logging level {manager_config.loglevel}")
+        print("*" * 10)
+        log_handler = logging.FileHandler(manager_config.logfile)
+        log_handler.setFormatter(formatter)
+        logging.getLogger().handlers = [log_handler]
 
     manager = ComputeManager(manager_config)
 
