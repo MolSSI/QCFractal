c38a5c71786c06418171c72c315543678d81d008
loriab <loriab@users.noreply.github.com>
2023-09-07 21:25:09 +0000

    Deploying to gh-pages from @ MolSSI/QCFractal@6586f0a158397b91252d95b4d6ebde6aa0844a5e 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.50b13.post33+geeb136b2',
+    VERSION: '0.50b13.post35+g6586f0a1',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
