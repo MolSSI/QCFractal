685ecfe13c52993fb74535fedab547ac87bfcea4
Benjamin Pritchard <ben@bennyp.org>
2023-09-09 20:43:10 -0400

    Make qcengine_list tolerant of qcengine not installed

--- qcfractalcompute/qcfractalcompute/run_scripts/qcengine_list.py
+++ qcfractalcompute/qcfractalcompute/run_scripts/qcengine_list.py
@@ -1,17 +1,20 @@
 import json
 
-import qcengine
-
 # This script returns the programs/procedures available in qcengine
 # as a dictionary of {program: version}
 # It is meant to be used with subprocess to get the available programs
 # in a conda environment
 
 if __name__ == "__main__":
-    r = qcengine.list_available_programs() | qcengine.list_available_procedures()
+    try:
+        import qcengine
 
-    progs = {x: qcengine.get_program(x).get_version() for x in qcengine.list_available_programs()}
-    procs = {x: qcengine.get_procedure(x).get_version() for x in qcengine.list_available_procedures()}
-    progs["qcengine"] = qcengine.__version__
+        progs = {x: qcengine.get_program(x).get_version() for x in qcengine.list_available_programs()}
+        procs = {x: qcengine.get_procedure(x).get_version() for x in qcengine.list_available_procedures()}
+        progs["qcengine"] = qcengine.__version__
+
+    except ImportError:
+        progs = {}
+        procs = {}
 
     print(json.dumps({**progs, **procs}))
