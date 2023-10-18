760363eca439df163ff76f3e7ba71c6093fe3d2b
bennybp <ben@bennyp.org>
2023-10-18 17:27:44 +0000

    Deploying to gh-pages from @ MolSSI/QCFractal@0e2f3ef0ab6d674926fd7c940c99ee9d6047a437 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.50.post79+gfc3cd304',
+    VERSION: '0.50.post85+g0e2f3ef0',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
