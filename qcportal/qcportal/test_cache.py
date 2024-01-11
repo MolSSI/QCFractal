6aafda8c6fce5616603890d9be0a21bfc9f6c950
Benjamin Pritchard <ben@bennyp.org>
2024-01-10 22:16:46 -0500

    Add more cache tests

---
+++ qcportal/qcportal/test_cache.py
@@ -0,0 +1,146 @@
+from __future__ import annotations
+
+import gc
+from typing import TYPE_CHECKING
+
+from qcportal.record_models import RecordStatusEnum
+from qcportal.singlepoint import SinglepointDataset
+from qcportal.singlepoint.test_dataset_models import test_specs, test_entries
+
+if TYPE_CHECKING:
+    from qcportal import PortalClient
+    from qcarchivetesting.testing_classes import QCATestingSnowflake
+
+
+def test_dataset_cache_basic(snowflake: QCATestingSnowflake, tmp_path):
+    cache_dir = tmp_path / "ptlcache"
+    client = snowflake.client(cache_dir=str(cache_dir))
+    ds: SinglepointDataset = client.add_dataset("singlepoint", "Test dataset")
+
+    ds.add_specification("spec_1", test_specs[0])
+    ds.add_specification("spec_2", test_specs[1])
+    ds.add_entries(test_entries[0])
+    ds.add_entries(test_entries[1])
+    ds.submit()
+    ds.fetch_records(include=["molecule"])
+
+    # reload the dataset
+    ds: SinglepointDataset = client.get_dataset("singlepoint", "Test dataset")
+    ds.propagate_client(None)
+
+    spec_map = {"spec_1": test_specs[0], "spec_2": test_specs[1]}
+    entry_map = {test_entries[0].name: test_entries[0], test_entries[1].name: test_entries[1]}
+    spec_names = {"spec_1", "spec_2"}
+    entry_names = {test_entries[0].name, test_entries[1].name}
+
+    # Specifications
+    assert set(ds._cache_data.get_specification_names()) == spec_names
+    cache_specs = ds._cache_data.get_all_specifications()
+    assert set(x.name for x in cache_specs) == spec_names
+
+    cache_specs = ds._cache_data.get_specifications(spec_names)
+    assert set(x.name for x in cache_specs) == spec_names
+
+    for sname, s in spec_map.items():
+        assert ds._cache_data.specification_exists(sname)
+        full_spec = ds._cache_data.get_specification(sname)
+        assert full_spec.name == sname
+        assert full_spec.specification == s
+
+    # Entries
+    assert set(ds._cache_data.get_entry_names()) == entry_names
+    cache_entries = ds._cache_data.get_entries(entry_names)
+    assert set(x.name for x in cache_entries) == entry_names
+
+    for ename, e in entry_map.items():
+        assert ds._cache_data.entry_exists(ename)
+        full_entry = ds._cache_data.get_entry(ename)
+        assert full_entry.name == e.name == ename
+        assert full_entry.molecule == e.molecule
+
+    # Records
+    for ename, e in entry_map.items():
+        for sname, s in spec_map.items():
+            assert ds._cache_data.record_exists(ename, sname)
+            r = ds._cache_data.get_record(ename, sname)
+            assert r is not None
+            assert r.specification == s
+            assert r.molecule == e.molecule
+
+            ri = ds._cache_data.get_record_info([ename], [sname], None)
+            assert len(ri) == 1
+
+            ri = ds._cache_data.get_record_info([ename], [sname], [RecordStatusEnum.waiting])
+            assert len(ri) == 1
+
+            assert ri[0][0] == ename
+            assert ri[0][1] == sname
+            assert ri[0][2] == r.id
+            assert ri[0][3] == r.status
+            assert ri[0][4] == r.modified_on
+
+    recs = ds._cache_data.get_records(entry_names, spec_names)
+    assert len(recs) == len(entry_names) * len(spec_names)
+
+    rinfo = ds._cache_data.get_record_info(entry_names, spec_names, [RecordStatusEnum.waiting])
+    assert len(rinfo) == len(entry_names) * len(spec_names)
+
+    rinfo = ds._cache_data.get_record_info(
+        entry_names, spec_names, [RecordStatusEnum.waiting, RecordStatusEnum.running]
+    )
+    assert len(rinfo) == len(entry_names) * len(spec_names)
+
+    rinfo = ds._cache_data.get_record_info(entry_names, spec_names, [RecordStatusEnum.complete])
+    assert len(rinfo) == 0
+
+
+def test_dataset_cache_update(snowflake_client: PortalClient):
+    ds: SinglepointDataset = snowflake_client.add_dataset("singlepoint", "Test dataset")
+
+    ds.add_specification("spec_1", test_specs[0])
+    ds.add_specification("spec_2", test_specs[1])
+    ds.add_entries(test_entries[0])
+    ds.add_entries(test_entries[1])
+    ds.submit()
+
+    r = ds.get_record(test_entries[0].name, "spec_1")
+    snowflake_client.cancel_records(r.id)
+    del r
+
+    # Cancel via client itself, not dataset
+
+    r = ds.get_record(test_entries[0].name, "spec_1")
+    assert r.status == RecordStatusEnum.cancelled
+    del r
+
+    r2 = ds._cache_data.get_record(test_entries[0].name, "spec_1")
+    assert r2.status == RecordStatusEnum.cancelled
+    del r2
+
+    # Test via iteration
+    r = ds.get_record(test_entries[1].name, "spec_2")
+    snowflake_client.cancel_records(r.id)
+    del r
+
+    for e, s, r in ds.iterate_records():
+        if e == test_entries[1].name and s == "spec_2":
+            assert r.status == RecordStatusEnum.cancelled
+
+
+def test_dataset_cache_writeback(snowflake_client: PortalClient):
+    ds: SinglepointDataset = snowflake_client.add_dataset("singlepoint", "Test dataset")
+
+    ds.add_specification("spec_1", test_specs[0])
+    ds.add_entries(test_entries[0])
+    ds.submit()
+    ds.fetch_records()
+
+    r = ds.get_record(test_entries[0].name, "spec_1")
+    assert r.molecule_ is None  # molecule not fetched
+    r._fetch_molecule()
+    assert r.molecule_ is not None  # molecule not fetched
+    del r  # should write back to the cache
+    gc.collect()
+
+    r2 = ds._cache_data.get_record(test_entries[0].name, "spec_1")
+    assert r2.molecule_ is not None
