799a475a4a54c156aeb096d66e10fd4b54fc964e
Benjamin Pritchard <ben@bennyp.org>
2024-03-20 21:10:48 -0400

    Better handling of missing tags/programs from managers

--- qcportal/qcportal/tasks/models.py
+++ qcportal/qcportal/tasks/models.py
@@ -1,9 +1,9 @@
 from typing import Dict, List
 
 try:
-    from pydantic.v1 import Field, BaseModel, constr, Extra
+    from pydantic.v1 import Field, BaseModel, constr, Extra, validator
 except ImportError:
-    from pydantic import Field, BaseModel, constr, Extra
+    from pydantic import Field, BaseModel, constr, Extra, validator
 
 from qcportal.base_models import RestModelBase
 from qcportal.managers import ManagerName
@@ -14,6 +14,23 @@ class TaskClaimBody(RestModelBase):
     programs: Dict[constr(to_lower=True), List[str]] = Field(..., description="Subset of programs to claim tasks for")
     tags: List[str] = Field(..., description="Subset of tags to claim tasks from")
     limit: int = Field(..., description="Limit on the number of tasks to claim")
+
+    @validator("tags")
+    def validate_tags(cls, v):
+        v = [x for x in v if len(x) > 0]
+
+        if len(v) == 0:
+            raise ValueError("'tags' field contains no non-zero-length tags")
+
+        return list(dict.fromkeys(v))  # remove duplicates, maintaining order (in python 3.6+)
+
+    @validator("programs")
+    def validate_programs(cls, v):
+        # Remove programs of zero length
+        v = {x: y for x, y in v.items() if len(x) > 0}
+        if len(v) == 0:
+            raise ValueError("'programs' field contains no non-zero-length programs")
+        return v
 
 
 class TaskReturnBody(RestModelBase):
