15be6097c50fb51090939c7692aad2d2f9657493
Benjamin Pritchard <ben@bennyp.org>
2024-12-19 11:37:24 -0500

    Update gridoptimization specification tests

--- qcfractal/qcfractal/components/gridoptimization/test_record_socket_specs.py
+++ qcfractal/qcfractal/components/gridoptimization/test_record_socket_specs.py
@@ -1,93 +1,7 @@
-from qcfractal.db_socket import SQLAlchemySocket
+from qcfractal.components.testing_fixtures import spec_test_runner
 from qcportal.gridoptimization import GridoptimizationSpecification, GridoptimizationKeywords
 from qcportal.optimization import OptimizationSpecification, OptimizationProtocols
 from qcportal.singlepoint import QCSpecification, SinglepointDriver, SinglepointProtocols
-
-
-def test_gridoptimization_socket_basic_specification(storage_socket: SQLAlchemySocket):
-    spec1 = GridoptimizationSpecification(
-        program="gridoptimization",
-        keywords=GridoptimizationKeywords(
-            preoptimization=False,
-            scans=[
-                {"type": "distance", "indices": [1, 2], "steps": [-0.1, 0.0], "step_type": "relative"},
-                {"type": "dihedral", "indices": [0, 1, 2, 3], "steps": [-90, 0], "step_type": "absolute"},
-            ],
-        ),
-        optimization_specification=OptimizationSpecification(
-            program="optprog1",
-            keywords={"k": "value"},
-            protocols=OptimizationProtocols(),
-            qc_specification=QCSpecification(
-                program="prog2",
-                driver="deferred",
-                method="b3lyp",
-                basis="6-31g",
-                keywords={"k2": "values2"},
-                protocols=SinglepointProtocols(wavefunction="all"),
-            ),
-        ),
-    )
-
-    spec2 = GridoptimizationSpecification(
-        program="gridoptimization",
-        keywords=GridoptimizationKeywords(
-            preoptimization=False,
-            scans=[
-                {"type": "dihedral", "indices": [0, 1, 2, 3], "steps": [-90, 0, 90], "step_type": "absolute"},
-            ],
-        ),
-        optimization_specification=OptimizationSpecification(
-            program="optprog2",
-            keywords={"k": "value"},
-            protocols=OptimizationProtocols(),
-            qc_specification=QCSpecification(
-                program="prog2",
-                driver=SinglepointDriver.hessian,
-                method="hf",
-                basis="def2-tzvp",
-                keywords={"k": "value"},
-                protocols=SinglepointProtocols(wavefunction="all"),
-            ),
-        ),
-    )
-
-    spec3 = GridoptimizationSpecification(
-        # Not putting in program
-        keywords=GridoptimizationKeywords(
-            preoptimization=False,
-            scans=[
-                {"type": "distance", "indices": [0, 3], "steps": [2.0, 3.0, 4.0], "step_type": "relative"},
-            ],
-        ),
-        optimization_specification=OptimizationSpecification(
-            program="optprog2",
-            keywords={"k": "value"},
-            protocols=OptimizationProtocols(trajectory="none"),
-            qc_specification=QCSpecification(
-                program="prog2",
-                driver=SinglepointDriver.hessian,
-                method="hf",
-                basis="def2-tzvp",
-                keywords={"k": "value"},
-                protocols=SinglepointProtocols(wavefunction="orbitals_and_eigenvalues"),
-            ),
-        ),
-    )
-
-    meta1, id1 = storage_socket.records.gridoptimization.add_specification(spec1)
-    meta2, id2 = storage_socket.records.gridoptimization.add_specification(spec2)
-    meta3, id3 = storage_socket.records.gridoptimization.add_specification(spec3)
-    assert meta1.success
-    assert meta2.success
-    assert meta3.success
-    assert meta1.inserted_idx == [0]
-    assert meta2.inserted_idx == [0]
-    assert meta3.inserted_idx == [0]
-    assert meta1.existing_idx == []
-    assert meta2.existing_idx == []
-    assert meta3.existing_idx == []
-
 
 common_opt_spec = OptimizationSpecification(
     program="optprog2",
@@ -104,7 +18,7 @@ common_opt_spec = OptimizationSpecification(
 )
 
 
-def test_gridoptimization_socket_add_specification_same_0(storage_socket: SQLAlchemySocket):
+def test_gridoptimization_socket_add_specification_same_1(spec_test_runner):
     spec1 = GridoptimizationSpecification(
         program="gridoptimization",
         keywords=GridoptimizationKeywords(
@@ -116,21 +30,10 @@ def test_gridoptimization_socket_add_specification_same_0(storage_socket: SQLAlc
         optimization_specification=common_opt_spec,
     )
 
-    meta, id = storage_socket.records.gridoptimization.add_specification(spec1)
-    assert meta.success
-    assert meta.inserted_idx == [0]
-    assert meta.existing_idx == []
-    assert id is not None
-
-    # Try inserting again
-    meta, id2 = storage_socket.records.gridoptimization.add_specification(spec1)
-    assert meta.success
-    assert meta.inserted_idx == []
-    assert meta.existing_idx == [0]
-    assert id == id2
+    spec_test_runner("gridoptimization", spec1, spec1, True)
 
 
-def test_gridoptimization_socket_add_specification_same_1(storage_socket: SQLAlchemySocket):
+def test_gridoptimization_socket_add_specification_same_2(spec_test_runner):
     # capitalization should be ignored
     spec1 = GridoptimizationSpecification(
         program="gridoptimization",
@@ -154,20 +57,10 @@ def test_gridoptimization_socket_add_specification_same_1(storage_socket: SQLAlc
         optimization_specification=common_opt_spec,
     )
 
-    meta, id = storage_socket.records.gridoptimization.add_specification(spec1)
-    assert meta.success
-    assert meta.inserted_idx == [0]
-    assert meta.existing_idx == []
-    assert id is not None
-
-    meta, id2 = storage_socket.records.gridoptimization.add_specification(spec2)
-    assert meta.success
-    assert meta.inserted_idx == []
-    assert meta.existing_idx == [0]
-    assert id == id2
+    spec_test_runner("gridoptimization", spec1, spec2, True)
 
 
-def test_gridoptimization_socket_add_specification_diff_1(storage_socket: SQLAlchemySocket):
+def test_gridoptimization_socket_add_specification_diff_1(spec_test_runner):
     # different indices
     spec1 = GridoptimizationSpecification(
         program="gridoptimization",
@@ -180,31 +73,14 @@ def test_gridoptimization_socket_add_specification_diff_1(storage_socket: SQLAlc
         optimization_specification=common_opt_spec,
     )
 
-    spec2 = GridoptimizationSpecification(
-        program="gridoptimization",
-        keywords=GridoptimizationKeywords(
-            preoptimization=False,
-            scans=[
-                {"type": "distance", "indices": [0, 4], "steps": [2.0, 3.0, 4.0], "step_type": "relative"},
-            ],
-        ),
-        optimization_specification=common_opt_spec,
+    go_kw = spec1.keywords.copy(
+        update={"scans": [{"type": "distance", "indices": [0, 4], "steps": [2.0, 3.0, 4.0], "step_type": "relative"}]}
     )
-
-    meta, id = storage_socket.records.gridoptimization.add_specification(spec1)
-    assert meta.success
-    assert meta.inserted_idx == [0]
-    assert meta.existing_idx == []
-    assert id is not None
-
-    meta, id2 = storage_socket.records.gridoptimization.add_specification(spec2)
-    assert meta.success
-    assert meta.inserted_idx == [0]
-    assert meta.existing_idx == []
-    assert id != id2
+    spec2 = spec1.copy(update={"keywords": go_kw})
+    spec_test_runner("gridoptimization", spec1, spec2, False)
 
 
-def test_gridoptimization_socket_add_specification_diff_2(storage_socket: SQLAlchemySocket):
+def test_gridoptimization_socket_add_specification_diff_2(spec_test_runner):
     # different steps
     spec1 = GridoptimizationSpecification(
         program="gridoptimization",
@@ -217,31 +93,14 @@ def test_gridoptimization_socket_add_specification_diff_2(storage_socket: SQLAlc
         optimization_specification=common_opt_spec,
     )
 
-    spec2 = GridoptimizationSpecification(
-        program="gridoptimization",
-        keywords=GridoptimizationKeywords(
-            preoptimization=False,
-            scans=[
-                {"type": "distance", "indices": [0, 3], "steps": [2.0, 3.0, 4.1], "step_type": "relative"},
-            ],
-        ),
-        optimization_specification=common_opt_spec,
+    go_kw = spec1.keywords.copy(
+        update={"scans": [{"type": "distance", "indices": [0, 3], "steps": [2.0, 3.0, 4.1], "step_type": "relative"}]}
     )
-
-    meta, id = storage_socket.records.gridoptimization.add_specification(spec1)
-    assert meta.success
-    assert meta.inserted_idx == [0]
-    assert meta.existing_idx == []
-    assert id is not None
-
-    meta, id2 = storage_socket.records.gridoptimization.add_specification(spec2)
-    assert meta.success
-    assert meta.inserted_idx == [0]
-    assert meta.existing_idx == []
-    assert id != id2
+    spec2 = spec1.copy(update={"keywords": go_kw})
+    spec_test_runner("gridoptimization", spec1, spec2, False)
 
 
-def test_gridoptimization_socket_add_specification_diff_3(storage_socket: SQLAlchemySocket):
+def test_gridoptimization_socket_add_specification_diff_3(spec_test_runner):
     # absolute vs relative
     spec1 = GridoptimizationSpecification(
         program="gridoptimization",
@@ -254,32 +113,15 @@ def test_gridoptimization_socket_add_specification_diff_3(storage_socket: SQLAlc
         optimization_specification=common_opt_spec,
     )
 
-    spec2 = GridoptimizationSpecification(
-        program="gridoptimization",
-        keywords=GridoptimizationKeywords(
-            preoptimization=False,
-            scans=[
-                {"type": "distance", "indices": [0, 3], "steps": [2.0, 3.0, 4.0], "step_type": "absolute"},
-            ],
-        ),
-        optimization_specification=common_opt_spec,
+    go_kw = spec1.keywords.copy(
+        update={"scans": [{"type": "distance", "indices": [0, 3], "steps": [2.0, 3.0, 4.0], "step_type": "absolute"}]}
     )
-
-    meta, id = storage_socket.records.gridoptimization.add_specification(spec1)
-    assert meta.success
-    assert meta.inserted_idx == [0]
-    assert meta.existing_idx == []
-    assert id is not None
-
-    meta, id2 = storage_socket.records.gridoptimization.add_specification(spec2)
-    assert meta.success
-    assert meta.inserted_idx == [0]
-    assert meta.existing_idx == []
-    assert id != id2
+    spec2 = spec1.copy(update={"keywords": go_kw})
+    spec_test_runner("gridoptimization", spec1, spec2, False)
 
 
-def test_gridoptimization_socket_add_specification_diff_4(storage_socket: SQLAlchemySocket):
-    # absolute vs relative
+def test_gridoptimization_socket_add_specification_diff_4(spec_test_runner):
+    # preoptimization
     spec1 = GridoptimizationSpecification(
         program="gridoptimization",
         keywords=GridoptimizationKeywords(
@@ -291,10 +133,17 @@ def test_gridoptimization_socket_add_specification_diff_4(storage_socket: SQLAlc
         optimization_specification=common_opt_spec,
     )
 
-    spec2 = GridoptimizationSpecification(
+    go_kw = spec1.keywords.copy(update={"preoptimization": True})
+    spec2 = spec1.copy(update={"keywords": go_kw})
+    spec_test_runner("gridoptimization", spec1, spec2, False)
+
+
+def test_gridoptimization_socket_add_specification_diff_5(spec_test_runner):
+    # basis set
+    spec1 = GridoptimizationSpecification(
         program="gridoptimization",
         keywords=GridoptimizationKeywords(
-            preoptimization=True,
+            preoptimization=False,
             scans=[
                 {"type": "distance", "indices": [0, 3], "steps": [2.0, 3.0, 4.0], "step_type": "relative"},
             ],
@@ -302,14 +151,26 @@ def test_gridoptimization_socket_add_specification_diff_4(storage_socket: SQLAlc
         optimization_specification=common_opt_spec,
     )
 
-    meta, id = storage_socket.records.gridoptimization.add_specification(spec1)
-    assert meta.success
-    assert meta.inserted_idx == [0]
-    assert meta.existing_idx == []
-    assert id is not None
+    qc_spec = spec1.optimization_specification.qc_specification.copy(update={"basis": "def2-qzvp"})
+    opt_spec = spec1.optimization_specification.copy(update={"qc_specification": qc_spec})
+    spec2 = spec1.copy(update={"optimization_specification": opt_spec})
+    spec_test_runner("gridoptimization", spec1, spec2, False)
 
-    meta, id2 = storage_socket.records.gridoptimization.add_specification(spec2)
-    assert meta.success
-    assert meta.inserted_idx == [0]
-    assert meta.existing_idx == []
-    assert id != id2
+
+def test_gridoptimization_socket_add_specification_diff_6(spec_test_runner):
+    # qc keywords
+    spec1 = GridoptimizationSpecification(
+        program="gridoptimization",
+        keywords=GridoptimizationKeywords(
+            preoptimization=False,
+            scans=[
+                {"type": "distance", "indices": [0, 3], "steps": [2.0, 3.0, 4.0], "step_type": "relative"},
+            ],
+        ),
+        optimization_specification=common_opt_spec,
+    )
+
+    qc_spec = spec1.optimization_specification.qc_specification.copy(update={"keywords": {"z": 1.0 - 10}})
+    opt_spec = spec1.optimization_specification.copy(update={"qc_specification": qc_spec})
+    spec2 = spec1.copy(update={"optimization_specification": opt_spec})
+    spec_test_runner("gridoptimization", spec1, spec2, False)
