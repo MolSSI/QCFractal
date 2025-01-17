ecc8ada893086be8c146bb97ec3a66df172eb0ae
Benjamin Pritchard <ben@bennyp.org>
2025-01-17 10:48:43 -0500

    Handle base_url through env as well

--- docker/jupyterhub_config.py
+++ docker/jupyterhub_config.py
@@ -3,7 +3,13 @@
 This authenticates using QCArchive server usernames and passwords.
 """
 
+import os
+
 c = get_config()  # noqa
+
+# Get the base url from the environment
+base_url = os.getenv("QCFRACTAL_JHUB_BASE_URL", "/")
+c.JupyterHub.base_url = base_url
 
 from qcarchive_authenticator import QCArchiveAuthenticator
 
@@ -16,6 +22,5 @@ c.JupyterHub.tornado_settings = {
 }
 
 c.JupyterHub.allow_named_servers = True
-c.JupyterHub.default_url = "/hub/home"
 
 c.Authenticator.allow_all = True
