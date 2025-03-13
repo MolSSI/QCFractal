29a8fb57c638438a8ec38e972ef4433596cd2912
jaclark5 <jennifer.clark@openforcefield.org>
2025-03-13 13:16:55 -0400

    black

--- qcportal/qcportal/external/scaffold.py
+++ qcportal/qcportal/external/scaffold.py
@@ -1,5 +1,11 @@
 """Functions to export and import elect information from QCFractal datasets
 
+This module will allow users to write and read a json file that can be reformed into an initial dataset
+**before** submission. This means that records are not present.
+
+Other functions are included for similar creation and importation from HDF5 files, although fewer fields
+are retained and transferred in this case.
+
 Organization: Open Molecular Software Foundation: Open Force Field
 Author: Jennifer A Clark
 Date: March 13 2025
@@ -12,30 +18,42 @@ from ..serialization import encode_to_json
 
 def to_json(ds, filename="scaffold.json", indent=4):
     """Export a QCFractal dataset to a json file.
-    
+
     Can be imported with :func:`fom_json` to make a new dataset.
 
     Args:
         ds (qcportal.*Dataset): QCFractal dataset
         filename (str, optional): Filename/path to store output json file. Defaults to "scaffold.json".
     """
-    
-    inputs = ["dataset_type", "name", "description", "tagline", "tags", "group", "provenance", "visibility",
-              "default_tag", "default_priority", "metadata", "owner_group"] # Inputs for client.add_dataset(
+
+    inputs = [
+        "dataset_type",
+        "name",
+        "description",
+        "tagline",
+        "tags",
+        "group",
+        "provenance",
+        "visibility",
+        "default_tag",
+        "default_priority",
+        "metadata",
+        "owner_group",
+    ]  # Inputs for client.add_dataset(
     metadata = {key: value for key, value in ds.dict().items() if key in inputs}
     d = {
-	    "metadata": metadata,
-	    "entries": {entry.name: entry for entry in ds.iterate_entries()},
-	    "specifications": ds.specifications
+        "metadata": metadata,
+        "entries": {entry.name: entry for entry in ds.iterate_entries()},
+        "specifications": ds.specifications,
     }
-    d_serializable = encode_to_json(d) # make everything serializable
+    d_serializable = encode_to_json(d)  # make everything serializable
     with open(filename, "w") as f:
         json.dump(d_serializable, f, indent=indent)
 
 
 def from_json(filename, client):
     """Create a QCFractal dataset from a json file.
-    
+
     Created from output of :func:`to_json`. This allows a user to save the "state"
     of a dataset before submission.
 
