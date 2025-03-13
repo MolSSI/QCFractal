f28db41a0f6f1a120618cf989f1bd5fc3d217f88
jaclark5 <jennifer.clark@openforcefield.org>
2025-03-13 17:26:37 -0400

    Add option to compress to bz2

--- qcportal/qcportal/external/scaffold.py
+++ qcportal/qcportal/external/scaffold.py
@@ -12,11 +12,12 @@ Date: March 13 2025
 """
 
 import json
+import bz2
 
 from ..serialization import encode_to_json
 
 
-def to_json(ds, filename="scaffold.json", indent=4):
+def to_json(ds, filename="scaffold.json", indent=4, compress=False):
     """Export a QCFractal dataset to a json file.
 
     Can be imported with :func:`fom_json` to make a new dataset.
@@ -24,6 +25,8 @@ def to_json(ds, filename="scaffold.json", indent=4):
     Args:
         ds (qcportal.*Dataset): QCFractal dataset
         filename (str, optional): Filename/path to store output json file. Defaults to "scaffold.json".
+        indent (int, optional): Level of indent for the output json file. Defaults to 4.
+        compress (bool, optional): If True, will compress to bz2. Defaults to False.
     """
 
     inputs = [
@@ -46,9 +49,14 @@ def to_json(ds, filename="scaffold.json", indent=4):
         "entries": {entry.name: entry for entry in ds.iterate_entries()},
         "specifications": ds.specifications,
     }
-    d_serializable = encode_to_json(d)  # make everything serializable
-    with open(filename, "w") as f:
-        json.dump(d_serializable, f, indent=indent)
+    d_serializable = encode_to_json(d)
+    
+    if compress:
+        with bz2.open(filename+".bz2", "wt", encoding="utf-8") as f:
+            json.dump(d_serializable, f, ensure_ascii=False, indent=indent)
+    else:
+        with open(filename, "w") as f:
+            json.dump(d_serializable, f, indent=indent)
 
 
 def from_json(filename, client):
@@ -65,8 +73,15 @@ def from_json(filename, client):
         qcportal.*Dataset: QCFractal dataset. This dataset is not submitted in this function.
     """
 
-    with open(filename, "r") as f:
-        ds_dict = json.load(f)
+    extension = filename.split(".")[-2:]
+    if extension[-1] == "json":
+        with open(filename, "r") as f:
+            ds_dict = json.load(f)
+    elif extension[-1] == "bz2" and extension[-2] == "json":
+        with bz2.open(filename, "rt", encoding="utf-8") as f:
+            ds_dict = json.load(f)
+    else:
+        raise ValueError(f"File extension must be json or json.bz2, not {extension[-2]}.{extension[-1]}")
 
     ds = client.add_dataset(**ds_dict["metadata"])
 
