9d71fb40d7d27c6377745e544cb50a0475dbd37e
jaclark5 <jennifer.clark@openforcefield.org>
2025-03-24 14:49:55 -0400

    Update testing

--- qcportal/qcportal/external/test_scaffold.py
+++ qcportal/qcportal/external/test_scaffold.py
@@ -6,7 +6,7 @@ from typing import TYPE_CHECKING, Optional
 
 import pytest
 
-from qcfractal.components.singlepoint.testing_helpers import load_test_data, run_test_data
+from qcfractal.components import singlepoint, optimization, torsiondrive, gridoptimization, manybody, reaction, neb
 from qcportal.molecules import Molecule
 from qcportal.singlepoint import SinglepointDataset
 from qcportal.external.scaffold import to_json, from_json
@@ -14,20 +14,59 @@ from qcportal.external.scaffold import to_json, from_json
 if TYPE_CHECKING:
     from qcarchivetesting.testing_classes import QCATestingSnowflake
 
-
-def test_dataset_client_query_dataset_records(snowflake: QCATestingSnowflake):
+@pytest.mark.parametrize(
+    "dataset_type", ["singlepoint", "optimization", "torsiondrive", "gridoptimization", "manybody", "reaction", "neb"]
+)
+def test_dataset_client_query_dataset_records(snowflake: QCATestingSnowflake, dataset_type: str):
     snowflake_client = snowflake.client()
     storage_socket = snowflake.get_storage_socket()
     manager_name, _ = snowflake.activate_manager()
 
-    ds: SinglepointDataset = snowflake_client.add_dataset("singlepoint", "Test dataset")
-    assert ds.status() == {}
-
-    input_spec, molecule, _ = load_test_data("sp_psi4_peroxide_energy_wfn")
-    run_test_data(storage_socket, manager_name, "sp_psi4_peroxide_energy_wfn")
-
+    if dataset_type == "singlepoint":
+        ds: SinglepointDataset = snowflake_client.add_dataset("singlepoint", "Test dataset")
+        assert ds.status() == {}
+    
+        input_spec, molecule, _ = singlepoint.testing_helpers.load_test_data("sp_psi4_peroxide_energy_wfn")
+        singlepoint.testing_helpers.run_test_data(storage_socket, manager_name, "sp_psi4_peroxide_energy_wfn")
+    elif dataset_type == "optimization":
+        ds = snowflake_client.add_dataset("optimization", "Test dataset")
+        assert ds.status() == {}
+        
+        input_spec, molecule, _ = optimization.testing_helpers.load_test_data("opt_psi4_benzene")
+        optimization.testing_helpers.run_test_data(storage_socket, manager_name, "opt_psi4_benzene")
+    elif dataset_type == "torsiondrive":
+        ds = snowflake_client.add_dataset("torsiondrive", "Test dataset")
+        assert ds.status() == {}
+        
+        input_spec, molecule, _ = torsiondrive.testing_helpers.load_test_data("td_H2O2_psi4_pbe")
+        torsiondrive.testing_helpers.run_test_data(storage_socket, manager_name, "td_H2O2_psi4_pbe")
+    elif dataset_type == "gridoptimization":
+        ds = snowflake_client.add_dataset("gridoptimization", "Test dataset")
+        assert ds.status() == {}
+        
+        input_spec, molecule, _ = gridoptimization.testing_helpers.load_test_data("go_H3NS_psi4_pbe")
+        gridoptimization.testing_helpers.run_test_data(storage_socket, manager_name, "go_H3NS_psi4_pbe")
+    elif dataset_type == "manybody":
+        ds = snowflake_client.add_dataset("manybody", "Test dataset")
+        assert ds.status() == {}
+        
+        input_spec, molecule, _ = manybody.testing_helpers.load_test_data("mb_all_he4_psi4_multi")
+        manybody.testing_helpers.run_test_data(storage_socket, manager_name, "mb_all_he4_psi4_multi")
+    elif dataset_type == "reaction":
+        ds = snowflake_client.add_dataset("reaction", "Test dataset")
+        assert ds.status() == {}
+        
+        input_spec, molecule, _ = reaction.testing_helpers.load_test_data("rxn_H2O_psi4_b3lyp_sp")
+        reaction.testing_helpers.run_test_data(storage_socket, manager_name, "rxn_H2O_psi4_b3lyp_sp")
+    elif dataset_type == "neb":
+        ds = snowflake_client.add_dataset("neb", "Test dataset")
+        assert ds.status() == {}
+        
+        input_spec, molecule, _ = neb.testing_helpers.load_test_data("neb_HCN_psi4_pbe")
+        neb.testing_helpers.run_test_data(storage_socket, manager_name, "neb_HCN_psi4_pbe")
+    
     molecule_2 = Molecule(symbols=["b"], geometry=[0, 0, 0])
-
+    
     # Add this as a part of the dataset
     ds.add_specification("spec_1", input_spec)
     ds.add_entry(name="test_molecule", molecule=molecule)
@@ -38,39 +77,37 @@ def test_dataset_client_query_dataset_records(snowflake: QCATestingSnowflake):
     assert os.path.exists(filename+".bz2")
     os.remove(filename+".bz2")
 
-    filename = "test_dataset.json"
     to_json(filename, ds)
     assert os.path.exists(filename)
     
     tmp_dict = json.load(open(filename, "r"))
     tmp_dict["metadata"]["name"] += "_test"
     json.dump(tmp_dict, open(filename, "w"))
-    ds = from_json(filename, snowflake_client)
+    ds2 = from_json(filename, snowflake_client)
     os.remove(filename)
+        
+    # Check records
+    if dataset_type == "singlepoint":
+        opt_hashes = {rec.molecule.get_hash() for _, _, rec in ds.iterate_records()}
+        opt_hashes_2 = {rec.molecule.get_hash() for _, _, rec in ds2.iterate_records()}
+    elif dataset_type == "optimization":
+        opt_hashes = {rec.initial_molecule().get_hash() for rec in ds.iterate_records()}
+        opt_hashes_2 = {rec.initial_molecule().get_hash() for rec in ds2.iterate_records()}
+    elif dataset_type == "torsiondrive":
+        opt_hashes = {rec.initial_molecules[0].get_hash() for _, _, rec in ds.iterate_records()}
+        opt_hashes_2 = {rec.initial_molecules[0].get_hash() for _, _, rec in ds2.iterate_records()}
+    elif dataset_type == "gridoptimization":
+        opt_hashes = {rec.initial_molecule().get_hash() for rec in ds.iterate_records()}
+        opt_hashes_2 = {rec.initial_molecule().get_hash() for rec in ds2.iterate_records()}
+    elif dataset_type == "manybody":
+        opt_hashes = {rec.initial_molecule().get_hash() for rec in ds.iterate_records()}
+        opt_hashes_2 = {rec.initial_molecule().get_hash() for rec in ds2.iterate_records()}
+    elif dataset_type == "reaction":
+        opt_hashes = {rec.components[0].singlepoint_record.molecule().get_hash() for rec in ds.iterate_records()}
+        opt_hashes_2 = {rec.components[0].singlepoint_record.molecule().get_hash() for rec in ds2.iterate_records()}
+    elif dataset_type == "neb":
+        opt_hashes = {list(rec.singlepoints.values())[0][0].molecule().get_hash() for rec in ds.iterate_records()}
+        opt_hashes_2 = {list(rec.singlepoints.values())[0][0].molecule().get_hash() for rec in ds2.iterate_records()}
 
-    # Query records belonging to a dataset
-    rec_id_2 = ds.get_record("test_molecule_2", "spec_1").id
-    mol_id_2 = ds.get_entry("test_molecule_2").molecule.id
-
-    query_res = snowflake_client.query_singlepoints(dataset_id=ds.id)
-    query_res_l = list(query_res)
-    assert len(query_res_l) == 2
-
-    query_res = snowflake_client.query_singlepoints(dataset_id=ds.id, molecule_id=mol_id_2)
-    query_res_l = list(query_res)
-    assert len(query_res_l) == 1
-    assert query_res_l[0].id == rec_id_2
-
-    query_res = snowflake_client.query_singlepoints(dataset_id=ds.id + 1, molecule_id=mol_id_2)
-    assert query_res._fetched == 0
-
-    # Query which dataset contains a record
-    rec_info = snowflake_client.query_dataset_records([rec_id_2])
-    assert len(rec_info) == 1
-    assert rec_info[0]["dataset_id"] == 1
-    assert rec_info[0]["entry_name"] == "test_molecule_2"
-
-    # Query which dataset contains a record
-    ds.remove_records(entry_names="test_molecule_2", specification_names="spec_1", delete_records=True)
-    rec_info = snowflake_client.query_dataset_records([rec_id_2])
-    assert len(rec_info) == 0
+    assert opt_hashes == opt_hashes_2
+        
