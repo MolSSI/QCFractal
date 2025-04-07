88d600a35ec03123334464267668b906e9d37c6d
Benjamin Pritchard <ben@bennyp.org>
2025-04-07 16:35:28 -0400

    Update permissions: serverinfo

--- qcfractal/qcfractal/flask_app/compute_v1/routes.py
+++ qcfractal/qcfractal/flask_app/compute_v1/routes.py
@@ -1,9 +1,9 @@
 from qcfractal.flask_app.compute_v1.blueprint import compute_v1
 from qcfractal.flask_app.helpers import get_public_server_information
-from qcfractal.flask_app.wrap_route import wrap_route
+from qcfractal.flask_app.wrap_route import wrap_global_route
 
 
 @compute_v1.route("/information", methods=["GET"])
-@wrap_route("READ")
+@wrap_global_route("information", "read")
 def get_information():
     return get_public_server_information()
