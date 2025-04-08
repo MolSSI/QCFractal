305cc44a2c116c4a309ac01f1c32c256ffd44a33
Benjamin Pritchard <ben@bennyp.org>
2025-04-08 08:42:10 -0400

    Update permissions: task claim/return

--- qcfractal/qcfractal/components/tasks/routes.py
+++ qcfractal/qcfractal/components/tasks/routes.py
@@ -2,7 +2,7 @@ from flask import current_app
 
 from qcfractal.flask_app import storage_socket
 from qcfractal.flask_app.compute_v1.blueprint import compute_v1
-from qcfractal.flask_app.wrap_route import wrap_route  # uses the same wrap_route as the user api
+from qcfractal.flask_app.wrap_route import wrap_global_route
 from qcportal.exceptions import LimitExceededError
 from qcportal.tasks import TaskClaimBody, TaskReturnBody
 from qcportal.utils import calculate_limit
@@ -11,7 +11,7 @@ from qcportal.utils import calculate_limit
 # WRITE action is not a mistake. Claim does some changes to the DB, so require
 # a bit more than read
 @compute_v1.route("/tasks/claim", methods=["POST"])
-@wrap_route("WRITE")
+@wrap_global_route("tasks", "claim")
 def claim_tasks_v1(body_data: TaskClaimBody):
     """Claims tasks from the task queue"""
 
@@ -27,7 +27,7 @@ def claim_tasks_v1(body_data: TaskClaimBody):
 
 
 @compute_v1.route("/tasks/return", methods=["POST"])
-@wrap_route("WRITE")
+@wrap_global_route("tasks", "return")
 def return_tasks_v1(body_data: TaskReturnBody):
     """Return finished tasks"""
 
