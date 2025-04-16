2416fb8e65225f1ef1495403c44b8c6b9b4605da
Benjamin Pritchard <ben@bennyp.org>
2025-04-16 09:28:57 -0400

    Merge migrations with db views branch

---
+++ qcfractal/qcfractal/alembic/versions/2025-04-16-56ef8ac1765b_merge_projects_and_db_views.py
@@ -0,0 +1,21 @@
+"""Merge projects and db views
+
+Revision ID: 56ef8ac1765b
+Revises: e98c86069950, 5ae71b845526
+Create Date: 2025-04-16 09:27:07.198910
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = "56ef8ac1765b"
+down_revision = ("e98c86069950", "5ae71b845526")
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
