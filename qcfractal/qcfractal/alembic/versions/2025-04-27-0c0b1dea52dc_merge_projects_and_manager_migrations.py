ec69df4fbe5f1ba9b8045e5695f60f2cc503f07a
Benjamin Pritchard <ben@bennyp.org>
2025-04-27 12:48:44 -0400

    Merge projects and manager migrations

---
+++ qcfractal/qcfractal/alembic/versions/2025-04-27-0c0b1dea52dc_merge_projects_and_manager_migrations.py
@@ -0,0 +1,21 @@
+"""Merge projects and manager migrations
+
+Revision ID: 0c0b1dea52dc
+Revises: a6a708376a1e, 93fa1a1b70f8
+Create Date: 2025-04-27 12:47:56.091604
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = "0c0b1dea52dc"
+down_revision = ("a6a708376a1e", "93fa1a1b70f8")
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
