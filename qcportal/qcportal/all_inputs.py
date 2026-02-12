25e0f8be7aacadbce70e1e552adbd5d187c376b2
Benjamin Pritchard <ben@bennyp.org>
2026-02-12 12:01:18 -0500

    [Pydantic v2] Update all inputs/results types

--- qcportal/qcportal/all_inputs.py
+++ qcportal/qcportal/all_inputs.py
@@ -1,29 +1,33 @@
-from .singlepoint.record_models import SinglepointInput
-from .optimization.record_models import OptimizationInput
-from .torsiondrive.record_models import TorsiondriveInput
+from typing import Annotated
+
+from pydantic import Field
+
 from .gridoptimization.record_models import GridoptimizationInput
-from .reaction.record_models import ReactionInput
 from .manybody.record_models import ManybodyInput
 from .neb.record_models import NEBInput
+from .optimization.record_models import OptimizationInput
+from .reaction.record_models import ReactionInput
+from .singlepoint.record_models import SinglepointInput
+from .torsiondrive.record_models import TorsiondriveInput
 
-from typing import Union
-
-AllInputTypes = Union[
-    SinglepointInput,
-    OptimizationInput,
-    TorsiondriveInput,
-    GridoptimizationInput,
-    ReactionInput,
-    ManybodyInput,
-    NEBInput,
+AllInputTypes = Annotated[
+    SinglepointInput
+    | OptimizationInput
+    | TorsiondriveInput
+    | GridoptimizationInput
+    | ReactionInput
+    | ManybodyInput
+    | NEBInput,
+    Field(discriminator="record_type"),
 ]
 
-AllQCPortalInputTypes = Union[
-    SinglepointInput,
-    OptimizationInput,
-    TorsiondriveInput,
-    GridoptimizationInput,
-    ReactionInput,
-    ManybodyInput,
-    NEBInput,
+AllQCPortalInputTypes = Annotated[
+    SinglepointInput
+    | OptimizationInput
+    | TorsiondriveInput
+    | GridoptimizationInput
+    | ReactionInput
+    | ManybodyInput
+    | NEBInput,
+    Field(discriminator="record_type"),
 ]
--- qcportal/qcportal/all_results.py
+++ qcportal/qcportal/all_results.py
@@ -1,29 +1,35 @@
-from typing import Union
+from typing import Annotated, Union
 
-# All possible records we can get from the server
-# These are all the possible result objects that might be returned by a manager
-from qcelemental.models import AtomicResult, OptimizationResult, FailedOperation
+from pydantic import Field
 
 from .generic_result import GenericTaskResult
 from .gridoptimization.record_models import GridoptimizationRecord
 from .manybody.record_models import ManybodyRecord
 from .neb.record_models import NEBRecord
 from .optimization.record_models import OptimizationRecord
+from .qcschema_v1 import AtomicResult, OptimizationResult, FailedOperation
 from .reaction.record_models import ReactionRecord
 from .singlepoint.record_models import SinglepointRecord
 from .torsiondrive.record_models import TorsiondriveRecord
 
-AllSchemaV1ResultTypes = Union[FailedOperation, AtomicResult, OptimizationResult, GenericTaskResult]
-AllQCPortalRecordTypes = Union[
-    SinglepointRecord,
-    OptimizationRecord,
-    TorsiondriveRecord,
-    GridoptimizationRecord,
-    ReactionRecord,
-    ManybodyRecord,
-    NEBRecord,
+# All possible records we can get from the server
+# These are all the possible result objects that might be returned by a manager
+
+# TODO - discriminator?
+AllSchemaV1ResultTypes = FailedOperation | AtomicResult | OptimizationResult | GenericTaskResult
+
+AllQCPortalRecordTypes = Annotated[
+    SinglepointRecord
+    | OptimizationRecord
+    | TorsiondriveRecord
+    | GridoptimizationRecord
+    | ReactionRecord
+    | ManybodyRecord
+    | NEBRecord,
+    Field(discriminator="record_type"),
 ]
 
+# TODO - discriminator?
 AllResultTypes = Union[
     FailedOperation,
     AtomicResult,
