a065de1b0870a625182cd49e74541d383b257e7d
Benjamin Pritchard <ben@bennyp.org>
2026-02-12 10:54:36 -0500

    [Pydantic v2] Migrate optimization models

--- qcportal/qcportal/optimization/dataset_models.py
+++ qcportal/qcportal/optimization/dataset_models.py
@@ -1,27 +1,24 @@
-from typing import Dict, Any, Union, Optional, Iterable
+from collections.abc import Iterable
+from typing import Any, Literal
 
-try:
-    from pydantic.v1 import BaseModel, Extra
-except ImportError:
-    from pydantic import BaseModel, Extra
-from typing_extensions import Literal
+from pydantic import BaseModel, ConfigDict
 
 from qcportal.dataset_models import BaseDataset
-from qcportal.metadata_models import InsertMetadata
 from qcportal.internal_jobs import InternalJob
+from qcportal.metadata_models import InsertMetadata
 from qcportal.molecules import Molecule
 from qcportal.optimization.record_models import OptimizationRecord, OptimizationSpecification
 
 
 class OptimizationDatasetNewEntry(BaseModel):
-    class Config:
-        extra = Extra.forbid
+
+    model_config = ConfigDict(extra="forbid")
 
     name: str
-    initial_molecule: Union[Molecule, int]
-    additional_keywords: Dict[str, Any] = {}
-    attributes: Dict[str, Any] = {}
-    comment: Optional[str] = None
+    initial_molecule: Molecule | int
+    additional_keywords: dict[str, Any] = {}
+    attributes: dict[str, Any] = {}
+    comment: str | None = None
 
 
 class OptimizationDatasetEntry(OptimizationDatasetNewEntry):
@@ -29,22 +26,20 @@ class OptimizationDatasetEntry(OptimizationDatasetNewEntry):
 
 
 class OptimizationDatasetSpecification(BaseModel):
-    class Config:
-        extra = Extra.forbid
+    model_config = ConfigDict(extra="forbid")
 
     name: str
     specification: OptimizationSpecification
-    description: Optional[str] = None
+    description: str | None = None
 
 
 class OptimizationDatasetRecordItem(BaseModel):
-    class Config:
-        extra = Extra.forbid
+    model_config = ConfigDict(extra="forbid")
 
     entry_name: str
     specification_name: str
     record_id: int
-    record: Optional[OptimizationRecord]
+    record: OptimizationRecord | None
 
 
 class OptimizationDataset(BaseDataset):
@@ -58,28 +53,28 @@ class OptimizationDataset(BaseDataset):
     _record_type = OptimizationRecord
 
     def add_specification(
-        self, name: str, specification: OptimizationSpecification, description: Optional[str] = None
+        self, name: str, specification: OptimizationSpecification, description: str | None = None
     ) -> InsertMetadata:
         spec = OptimizationDatasetSpecification(name=name, specification=specification, description=description)
         return self._add_specifications(spec)
 
     def add_entries(
-        self, entries: Union[OptimizationDatasetNewEntry, Iterable[OptimizationDatasetNewEntry]]
+        self, entries: OptimizationDatasetNewEntry | Iterable[OptimizationDatasetNewEntry]
     ) -> InsertMetadata:
         return self._add_entries(entries)
 
     def background_add_entries(
-        self, entries: Union[OptimizationDatasetNewEntry, Iterable[OptimizationDatasetNewEntry]]
+        self, entries: OptimizationDatasetNewEntry | Iterable[OptimizationDatasetNewEntry]
     ) -> InternalJob:
         return self._background_add_entries(entries)
 
     def add_entry(
         self,
         name: str,
-        initial_molecule: Union[Molecule, int],
-        additional_keywords: Optional[Dict[str, Any]] = None,
-        attributes: Optional[Dict[str, Any]] = None,
-        comment: Optional[str] = None,
+        initial_molecule: Molecule | int,
+        additional_keywords: dict[str, Any] | None = None,
+        attributes: dict[str, Any] | None = None,
+        comment: str | None = None,
     ):
         if additional_keywords is None:
             additional_keywords = {}
