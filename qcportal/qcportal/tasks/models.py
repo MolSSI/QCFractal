0f75acb04ecce7921d7b8d3b8911e4347e5aedfa
Benjamin Pritchard <ben@bennyp.org>
2026-02-12 10:02:57 -0500

    [Pydantic v2] Migrate task request models

--- qcportal/qcportal/tasks/models.py
+++ qcportal/qcportal/tasks/models.py
@@ -1,21 +1,18 @@
-from typing import Dict, List
-
-try:
-    from pydantic.v1 import Field, BaseModel, constr, Extra, validator, root_validator
-except ImportError:
-    from pydantic import Field, BaseModel, constr, Extra, validator, root_validator
+from pydantic import Field, model_validator, field_validator
 
 from qcportal.base_models import RestModelBase
+from qcportal.common_types import LowerStr
 from qcportal.managers import ManagerName
 
 
 class TaskClaimBody(RestModelBase):
     name_data: ManagerName = Field(..., description="Name information about this manager")
-    programs: Dict[constr(to_lower=True), List[str]] = Field(..., description="Subset of programs to claim tasks for")
-    compute_tags: List[str] = Field(..., description="Subset of tags to claim tasks from")
+    programs: dict[LowerStr, list[str]] = Field(..., description="Subset of programs to claim tasks for")
+    compute_tags: list[str] = Field(..., description="Subset of tags to claim tasks from")
     limit: int = Field(..., description="Limit on the number of tasks to claim")
 
-    @validator("compute_tags")
+    @field_validator("compute_tags", mode="after")
+    @classmethod
     def validate_tags(cls, v):
         v = [x for x in v if len(x) > 0]
 
@@ -24,7 +21,8 @@ class TaskClaimBody(RestModelBase):
 
         return list(dict.fromkeys(v))  # remove duplicates, maintaining order (in python 3.6+)
 
-    @validator("programs")
+    @field_validator("programs", mode="after")
+    @classmethod
     def validate_programs(cls, v):
         # Remove programs of zero length
         v = {x: y for x, y in v.items() if len(x) > 0}
@@ -32,15 +30,15 @@ class TaskClaimBody(RestModelBase):
             raise ValueError("'programs' field contains no non-zero-length programs")
         return v
 
-    # TODO - DEPRECATED - remove at some point
-    @root_validator(pre=True)
+    @model_validator(mode="before")
+    @classmethod
     def _old_tags(cls, values):
-        if "tags" in values:
-            values["compute_tags"] = values.pop("tags")
-
+        if isinstance(values, dict):
+            if "tags" in values:
+                values["compute_tags"] = values.pop("tags")
         return values
 
 
 class TaskReturnBody(RestModelBase):
     name_data: ManagerName = Field(..., description="Name information about this manager")
-    results_compressed: Dict[int, bytes]
+    results_compressed: dict[int, bytes]
