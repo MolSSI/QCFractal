a2a537368cc2b917a965e20f795b39d7225cae73
Benjamin Pritchard <ben@bennyp.org>
2026-02-12 10:13:21 -0500

    [Pydantic v2] Migrate molecule stuff

--- qcportal/qcportal/molecules/models.py
+++ qcportal/qcportal/molecules/models.py
@@ -1,22 +1,18 @@
-from typing import Optional, List, Dict
-
-from qcelemental.models.molecule import Identifiers as MoleculeIdentifiers
-
 from ..base_models import QueryModelBase, RestModelBase, QueryIteratorBase
-from ..qcschema_v1 import Molecule
+from ..qcschema_v1 import Molecule, MoleculeIdentifiers
 
 
 class MoleculeQueryFilters(QueryModelBase):
-    molecule_id: Optional[List[int]] = None
-    molecule_hash: Optional[List[str]] = None
-    molecular_formula: Optional[List[str]] = None
-    identifiers: Optional[Dict[str, List[str]]] = None
+    molecule_id: list[int] | None = None
+    molecule_hash: list[str] | None = None
+    molecular_formula: list[str] | None = None
+    identifiers: dict[str, list[str]] | None = None
 
 
 class MoleculeModifyBody(RestModelBase):
-    name: Optional[str] = None
-    comment: Optional[str] = None
-    identifiers: Optional[MoleculeIdentifiers] = None
+    name: str | None = None
+    comment: str | None = None
+    identifiers: MoleculeIdentifiers | None = None
     overwrite_identifiers: bool = False
 
 
@@ -47,11 +43,11 @@ class MoleculeQueryIterator(QueryIteratorBase[Molecule]):
         api_limit = client.api_limits["get_molecules"] // 4
         QueryIteratorBase.__init__(self, client, query_filters, api_limit)
 
-    def _request(self) -> List[Molecule]:
+    def _request(self) -> list[Molecule]:
         molecule_ids = self._client.make_request(
             "post",
             "api/v1/molecules/query",
-            List[int],
+            list[int],
             body_model=MoleculeQueryFilters,
             body=self._query_filters,
         )
