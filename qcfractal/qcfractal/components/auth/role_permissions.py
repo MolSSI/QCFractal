314600dab859aabdebc632d6a1e86af6d7bc2c9c
Benjamin Pritchard <ben@bennyp.org>
2026-03-13 09:51:55 -0400

    Rework wrap_route and RBAC internals

--- qcfractal/qcfractal/components/auth/role_permissions.py
+++ qcfractal/qcfractal/components/auth/role_permissions.py
@@ -1,64 +1,20 @@
-from typing import Dict, Set
+import os
+from enum import Enum
 
-GLOBAL_ROLE_PERMISSIONS: Dict[str, Dict[str, Set[str]]] = {
-    "admin": {
-        "*": {"*"},
-    },
-    "maintain": {
-        "information": {"read", "modify"},
-        "me": {"read", "modify"},
-        "users": {"read", "modify"},
-        "groups": {"*"},
-        "access_log": {"*"},
-        "server_errors": {"*"},
-        "internal_jobs": {"*"},
-        "managers": {"read"},
-        "records": {"*"},
-        "datasets": {"*"},
-        "projects": {"*"},
-    },
-    "monitor": {
-        "information": {"read"},
-        "me": {"read", "modify"},
-        "users": {"read", "modify"},  # TODO - DEPRECATE
-        "managers": {"read"},
-        "records": {"read"},
-        "datasets": {"read"},
-        "projects": {"read"},
-        "access_log": {"read"},
-        "server_errors": {"read"},
-        "internal_jobs": {"read"},
-    },
-    "submit": {
-        "information": {"read"},
-        "me": {"read", "modify"},
-        "users": {"read", "modify"},  # TODO - DEPRECATE
-        "managers": {"read"},
-        "records": {"read", "add", "modify", "delete"},
-        "datasets": {"read", "add", "modify", "delete", "create_view"},
-        "projects": {"read", "add", "modify", "delete"},
-    },
-    "read": {
-        "information": {"read"},
-        "me": {"read", "modify"},
-        "users": {"read", "modify"},  # TODO - DEPRECATE
-        "managers": {"read"},
-        "records": {"read"},
-        "datasets": {"read"},
-        "projects": {"read"},
-    },
-    "anonymous": {
-        "information": {"read"},
-        "managers": {"read"},
-        "records": {"read"},
-        "datasets": {"read"},
-        "projects": {"read"},
-    },
-    "compute": {
-        "information": {"read"},
-        "me": {"read", "modify"},
-        "managers": {"add", "modify"},
-        "tasks": {"claim", "return"},
-        "users": {"read", "modify"},  # TODO - DEPRECATE
-    },
-}
+import yaml
+from pydantic import TypeAdapter
+
+
+class AuthorizedEnum(str, Enum):
+    Allow = "Allow"
+    Deny = "Deny"
+    Conditional = "Conditional"
+
+
+_my_path = os.path.dirname(os.path.abspath(__file__))
+_global_perm_file_path = os.path.join(_my_path, "global_role_permissions.yaml")
+
+with open(_global_perm_file_path, "r") as f:
+    file_data = yaml.safe_load(f)
+
+GLOBAL_ROLE_PERMISSIONS = TypeAdapter(dict[str, dict[str, dict[str, AuthorizedEnum]]]).validate_python(file_data)
