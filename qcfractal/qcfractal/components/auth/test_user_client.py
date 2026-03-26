8590710a7712eb6adfe960e6842583b71ab10ad2
Benjamin Pritchard <ben@bennyp.org>
2026-03-26 18:43:36 -0400

    Update permissions handling in routes and update tests

--- qcfractal/qcfractal/components/auth/test_user_client.py
+++ qcfractal/qcfractal/components/auth/test_user_client.py
@@ -1,5 +1,3 @@
-from typing import Optional
-
 import pytest
 
 from qcarchivetesting import test_users
@@ -16,7 +14,7 @@ from .test_user_socket import invalid_usernames, invalid_passwords
 
 
 def test_user_client_list(secure_snowflake: QCATestingSnowflake):
-    client = secure_snowflake.client("admin_user", test_users["admin_user"]["pw"])
+    client = secure_snowflake.user_client("admin_user")
     users = client.list_users()
 
     assert len(users) == len(test_users)
@@ -32,7 +30,7 @@ def test_user_client_list(secure_snowflake: QCATestingSnowflake):
 
 
 def test_user_client_get(secure_snowflake: QCATestingSnowflake):
-    client = secure_snowflake.client("admin_user", test_users["admin_user"]["pw"])
+    client = secure_snowflake.user_client("admin_user")
 
     for username, uinfo in test_users.items():
         u = client.get_user(username)
@@ -46,7 +44,7 @@ def test_user_client_get(secure_snowflake: QCATestingSnowflake):
 
 def test_user_client_get_me(secure_snowflake: QCATestingSnowflake):
     for username, uinfo in test_users.items():
-        client = secure_snowflake.client(username, uinfo["pw"])
+        client = secure_snowflake.user_client(username)
         me = client.get_user()
 
         assert me.username == username
@@ -54,7 +52,7 @@ def test_user_client_get_me(secure_snowflake: QCATestingSnowflake):
 
 
 def test_user_client_add(secure_snowflake: QCATestingSnowflake):
-    client = secure_snowflake.client("admin_user", test_users["admin_user"]["pw"])
+    client = secure_snowflake.user_client("admin_user")
 
     uinfo = UserInfo(
         username="george",
@@ -71,7 +69,7 @@ def test_user_client_add(secure_snowflake: QCATestingSnowflake):
 
 
 def test_user_client_add_existing(secure_snowflake: QCATestingSnowflake):
-    client = secure_snowflake.client("admin_user", test_users["admin_user"]["pw"])
+    client = secure_snowflake.user_client("admin_user")
 
     uinfo = UserInfo(
         username="read_user",
@@ -87,7 +85,7 @@ def test_user_client_add_existing(secure_snowflake: QCATestingSnowflake):
 
 
 def test_user_client_add_badrole(secure_snowflake: QCATestingSnowflake):
-    client = secure_snowflake.client("admin_user", test_users["admin_user"]["pw"])
+    client = secure_snowflake.user_client("admin_user")
 
     uinfo = UserInfo(
         username="read_user",
@@ -103,7 +101,7 @@ def test_user_client_add_badrole(secure_snowflake: QCATestingSnowflake):
 
 
 def test_user_client_add_badgroup(secure_snowflake: QCATestingSnowflake):
-    client = secure_snowflake.client("admin_user", test_users["admin_user"]["pw"])
+    client = secure_snowflake.user_client("admin_user")
 
     uinfo = UserInfo(
         username="read_user_2",
@@ -117,7 +115,7 @@ def test_user_client_add_badgroup(secure_snowflake: QCATestingSnowflake):
 
 
 def test_user_client_use_nonexist(secure_snowflake: QCATestingSnowflake):
-    client = secure_snowflake.client("admin_user", test_users["admin_user"]["pw"])
+    client = secure_snowflake.user_client("admin_user")
 
     uinfo = client.get_user()
     uinfo.__dict__["id"] = 1234  # bypass pydantic validation
@@ -133,7 +131,7 @@ def test_user_client_use_nonexist(secure_snowflake: QCATestingSnowflake):
 
 
 def test_user_client_use_invalid_username(secure_snowflake: QCATestingSnowflake):
-    client = secure_snowflake.client("admin_user", test_users["admin_user"]["pw"])
+    client = secure_snowflake.user_client("admin_user")
 
     for username in invalid_usernames:
         uinfo = client.get_user()
@@ -153,7 +151,7 @@ def test_user_client_use_invalid_username(secure_snowflake: QCATestingSnowflake)
 
 
 def test_user_client_use_invalid_groupname(secure_snowflake: QCATestingSnowflake):
-    client = secure_snowflake.client("admin_user", test_users["admin_user"]["pw"])
+    client = secure_snowflake.user_client("admin_user")
 
     for groupname in invalid_groupnames:
         uinfo = client.get_user()
@@ -167,7 +165,7 @@ def test_user_client_use_invalid_groupname(secure_snowflake: QCATestingSnowflake
 
 
 def test_user_client_use_invalid_password(secure_snowflake: QCATestingSnowflake):
-    client = secure_snowflake.client("admin_user", test_users["admin_user"]["pw"])
+    client = secure_snowflake.user_client("admin_user")
 
     for password in invalid_passwords:
         uinfo = client.get_user()
@@ -180,7 +178,7 @@ def test_user_client_use_invalid_password(secure_snowflake: QCATestingSnowflake)
 
 
 def test_user_client_delete(secure_snowflake: QCATestingSnowflake):
-    client = secure_snowflake.client("admin_user", test_users["admin_user"]["pw"])
+    client = secure_snowflake.user_client("admin_user")
 
     client.delete_user("read_user")
 
@@ -189,14 +187,14 @@ def test_user_client_delete(secure_snowflake: QCATestingSnowflake):
 
 
 def test_user_client_delete_self(secure_snowflake: QCATestingSnowflake):
-    client = secure_snowflake.client("admin_user", test_users["admin_user"]["pw"])
+    client = secure_snowflake.user_client("admin_user")
 
     with pytest.raises(PortalRequestError, match=r"Cannot delete your own user"):
         client.delete_user("admin_user")
 
 
 def test_user_client_modify(secure_snowflake: QCATestingSnowflake):
-    client = secure_snowflake.client("admin_user", test_users["admin_user"]["pw"])
+    client = secure_snowflake.user_client("admin_user")
     uinfo = client.get_user("read_user")
 
     uinfo.fullname = "New Full Name"
@@ -221,9 +219,10 @@ def test_user_client_modify(secure_snowflake: QCATestingSnowflake):
 
 
 def test_user_client_modify_self(secure_snowflake: QCATestingSnowflake):
-    client = secure_snowflake.client("read_user", test_users["read_user"]["pw"])
+    client = secure_snowflake.user_client("read_user")
     uinfo = client.get_user("read_user")
 
+    # Attempts to change own role
     uinfo.fullname = "New Full Name"
     uinfo.organization = "New Organization"
     uinfo.email = "New Email"
@@ -247,35 +246,8 @@ def test_user_client_modify_self(secure_snowflake: QCATestingSnowflake):
     assert uinfo3.groups == ["group2"]
 
 
-def test_user_client_modify_self_me(secure_snowflake: QCATestingSnowflake):
-    # Similar to above, but using the /me endpoint
-    client = secure_snowflake.client("read_user", test_users["read_user"]["pw"])
-    uinfo = client.get_user("read_user")
-
-    uinfo.fullname = "New Full Name"
-    uinfo.organization = "New Organization"
-    uinfo.email = "New Email"
-    uinfo.role = "submit"
-    uinfo.groups = ["group3"]
-    uinfo.enabled = False
-
-    uinfo2 = client.make_request("patch", f"api/v1/me", UserInfo, body=uinfo)
-    uinfo3 = client.get_user("read_user")
-    assert uinfo2 == uinfo3
-
-    # Only some fields changed
-    assert uinfo3.fullname == "New Full Name"
-    assert uinfo3.organization == "New Organization"
-    assert uinfo3.email == "New Email"
-
-    # Did not change
-    assert uinfo3.role == "read"
-    assert uinfo3.enabled is True
-    assert uinfo3.groups == ["group2"]
-
-
 def test_user_client_modify_badrole(secure_snowflake: QCATestingSnowflake):
-    client = secure_snowflake.client("admin_user", test_users["admin_user"]["pw"])
+    client = secure_snowflake.user_client("admin_user")
     uinfo = client.get_user("read_user")
 
     uinfo.fullname = "New Full Name"
@@ -289,7 +261,7 @@ def test_user_client_modify_badrole(secure_snowflake: QCATestingSnowflake):
 
 
 def test_user_client_modify_badgroup(secure_snowflake: QCATestingSnowflake):
-    client = secure_snowflake.client("admin_user", test_users["admin_user"]["pw"])
+    client = secure_snowflake.user_client("admin_user")
     uinfo = client.get_user("read_user")
 
     uinfo.fullname = "New Full Name"
@@ -307,7 +279,7 @@ def test_user_client_change_password(secure_snowflake: QCATestingSnowflake):
     with pytest.raises(AuthenticationFailure):
         secure_snowflake.client("read_user", "a_new_password1234")
 
-    client = secure_snowflake.client("admin_user", test_users["admin_user"]["pw"])
+    client = secure_snowflake.user_client("admin_user")
 
     new_pw = client.change_user_password("read_user", "a_new_password1234")
 
@@ -319,7 +291,7 @@ def test_user_client_change_password(secure_snowflake: QCATestingSnowflake):
 
 
 def test_user_client_change_password_self(secure_snowflake: QCATestingSnowflake):
-    client = secure_snowflake.client("read_user", test_users["read_user"]["pw"])
+    client = secure_snowflake.user_client("read_user")
 
     new_pw = client.change_user_password(None, "a_new_password1234")
     assert new_pw == "a_new_password1234"
@@ -330,47 +302,8 @@ def test_user_client_change_password_self(secure_snowflake: QCATestingSnowflake)
     secure_snowflake.client("read_user", new_pw)
 
 
-def test_user_client_change_password_self_me(secure_snowflake: QCATestingSnowflake):
-    # Similar to above, but using the /me endpoint
-    # we don't expose this in qcportal, so manually do make_request
-
-    client = secure_snowflake.client("read_user", test_users["read_user"]["pw"])
-
-    new_password = "a_new_password1234"
-    new_pw = client.make_request("put", f"api/v1/me/password", str, body_model=str, body=new_password)
-    assert new_pw == "a_new_password1234"
-
-    secure_snowflake.client("read_user", "a_new_password1234")
-
-    # Reset password
-    new_pw_2 = client.make_request("put", f"api/v1/me/password", str, body_model=Optional[str], body=None)
-    secure_snowflake.client("read_user", new_pw_2)
-
-    with pytest.raises(AuthenticationFailure, match="Incorrect username or password"):
-        secure_snowflake.client("read_user", new_pw)
-
-
 def test_user_client_get_set_preferences(secure_snowflake: QCATestingSnowflake):
-    client = secure_snowflake.client("admin_user", test_users["admin_user"]["pw"])
-    user_id = client.get_user("read_user").id
-
-    prefs = {"favorites": [4, 5, 2], "mode": "dark"}
-
-    client.make_request("put", f"api/v1/users/{user_id}/preferences", None, body_model=dict, body=prefs)
-
-    server_prefs = client.make_request("get", f"api/v1/users/{user_id}/preferences", dict)
-    assert server_prefs == prefs
-
-    prefs["new_pref"] = "value"
-
-    client.make_request("put", f"api/v1/users/{user_id}/preferences", None, body_model=dict, body=prefs)
-
-    server_prefs = client.make_request("get", f"api/v1/users/{user_id}/preferences", dict)
-    assert server_prefs == prefs
-
-
-def test_user_client_set_preferences_self(secure_snowflake: QCATestingSnowflake):
-    client = secure_snowflake.client("read_user", test_users["read_user"]["pw"])
+    client = secure_snowflake.user_client("admin_user")
     user_id = client.get_user("read_user").id
 
     prefs = {"favorites": [4, 5, 2], "mode": "dark"}
@@ -406,3 +339,27 @@ def test_user_client_set_preferences_self_me(secure_snowflake: QCATestingSnowfla
 
     server_prefs = client.make_request("get", f"api/v1/me/preferences", dict)
     assert server_prefs == prefs
+
+
+def test_user_client_modify_other_user(secure_snowflake: QCATestingSnowflake):
+    # We manually use the /me endpoints, but with malformed data
+    # This makes sure that we can't modify other users through that endpoint
+
+    reader_client = secure_snowflake.user_client("read_user")
+    submit_client = secure_snowflake.user_client("submit_user")
+
+    reader_uinfo = reader_client.get_user()
+    submit_uinfo = submit_client.get_user()
+
+    # Change the id and username to match ours
+    new_uinfo = submit_uinfo.model_copy(update={"id": reader_uinfo.id})
+    with pytest.raises(PortalRequestError, match=r"Forbidden"):
+        submit_client.make_request("patch", f"api/v1/me", None, body=new_uinfo)
+
+    new_uinfo = submit_uinfo.model_copy(update={"username": reader_uinfo.username})
+    with pytest.raises(PortalRequestError, match=r"Forbidden"):
+        submit_client.make_request("patch", f"api/v1/me", None, body=new_uinfo)
+
+    new_uinfo = submit_uinfo.model_copy(update={"id": reader_uinfo.id, "username": reader_uinfo.username})
+    with pytest.raises(PortalRequestError, match=r"Forbidden"):
+        submit_client.make_request("patch", f"api/v1/me", None, body=new_uinfo)
