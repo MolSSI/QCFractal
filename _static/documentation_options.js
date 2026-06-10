4013440c89582bbba6d66ec88d167291020b8f89
bennybp <ben@bennyp.org>
2026-06-10 13:42:34 +0000

    Deploying to gh-pages from @ MolSSI/QCFractal@e8c24b5337945bebd641919a44deaa4ee46447fb 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.65.post58+g8bf386967',
+    VERSION: '0.65.post70+ge8c24b533',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
